c80c216ff8a87bec2079a8ba3bc11a2fd7be2bdc
Brigitte Markmann <brigitte.markmann@ferrous-systems.com>
2023-01-11 16:31:19 +0100

    Try clap.

--- sudo/src/main.rs
+++ sudo/src/main.rs
@@ -1,1 +1,23 @@
-fn main() {}
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+#[derive(Debug)]
+struct CustomError(String);
+
+// fn main() {
+fn main() -> Result<(), CustomError> {
+
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+    .map_err(|err| CustomError(format!("Error reading `{}`: {}", &args.path.display(), err)))?;
+    println!("file content: {}", content);
+    Ok(())
+}
