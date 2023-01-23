e9613c6d077b7d854421982f50a739740684e52a
Brigitte Markmann <brigitte.markmann@ferrous-systems.com>
2023-01-23 15:01:36 +0100

    Adds all Options.

--- sudo/src/cli_args.rs
+++ sudo/src/cli_args.rs
@@ -1,50 +1,79 @@
-use std::{path::PathBuf, error::Error};
+use std::path::PathBuf;
 
 use clap:: {
     Parser,
+    ArgAction
 };
 
-// warning: derive helper attribute is used before it is introduced
-#[command(name = "sudo-rs")]
-#[command(about = "sudo - execute a command as another user", long_about = None)]
-#[command(version)] // Read from `Cargo.toml`
-
+#[clap(
+    name = "sudo-rs",
+    about = "sudo - execute a command as another user",
+    version,
+    // disable_version_flag = true,
+    // disable_help_flag = true,
+    override_usage = "usage: sudo -h | -K | -k | -V
+    usage: sudo -v [-AknS] [-g group] [-h host] [-p prompt] [-u user]
+    usage: sudo -l [-AknS] [-g group] [-h host] [-p prompt] [-U user] [-u user] [command]
+    usage: sudo [-AbEHknPS] [-C num] [-D directory] [-g group] [-h host] [-p prompt] [-R directory] [-T timeout] [-u user] [VAR=value] [-i|-s] [<command>]
+    usage: sudo -e [-AknS] [-C num] [-D directory] [-g group] [-h host] [-p prompt] [-R directory] [-T timeout] [-u user] file ...",
+)]
 #[derive(Debug, Parser)]
-pub struct Cli {
-    /// This comment will be printed in help!
-    // when `Option<>` isn't used, the arg is required
-    pattern: Option<String>,
-    /// The path to the file to read
-    #[arg(short, help = "This overrides the comment!")]
-    pub path: Option<PathBuf>,
-    /// A flag
-    // `action` sets ArgAction::SetTrue, means we don't need to set a value when we have a bool.
-    #[arg(long, short, action)] 
-    pub flag: bool,
-    /// change the working directory before running command
-    #[arg(long = "chdir=directory", short = 'D', action, conflicts_with("list"))] // exclusion
-    pub directory: bool,
-    #[arg(long, short, action)] 
+pub struct Cli {   
+    #[arg(long, short = 'A', help = "use a helper program for password prompting", action)]  
+    pub askpass: bool,
+    #[arg(short = 'b', long, help = "run command in the background", action)]
+    pub background: bool,
+    #[arg(short = 'B', long, help = "ring bell when prompting", action)]
+    pub bell: bool,
+    #[arg(short = 'C', long = "close-from=num", help = "close all file descriptors >= num")]
+    pub num: Option<i16>,
+    #[arg(short = 'D', long = "chdir=directory", help = "change the working directory before running command")]
+    pub directory:  Option<PathBuf>,
+    #[arg(short = 'E', long = "preserve-env", help = "preserve user environment when running command", action)]
+    pub preserve_env: bool,
+    #[arg(long = "preserve-env=list", help = "preserve specific environment variables", value_name = "list")]
+    pub preserve_env_list: Option<String>,
+    #[arg(short = 'e', long, help = "edit files instead of running a command", action)]
+    pub edit: bool,
+    #[arg(short = 'g', long = "group=group", help = "run command as the specified group name or ID")]
+    pub group: Option<String>,
+    #[arg(short = 'H', long = "set-home", help = "set HOME variable to target user's home dir", action)]
+    pub set_home: bool,
+    // #[arg(long, help = "display help message and exit!", action = ArgAction::Help)] 
+    // pub help: bool, // TO DO: help as well as host are supposed to have short 'h'???
+    // #[arg(short = 'h', long = "host", help = "run command on host (if supported by plugin)")]
+    // pub host: Option<String>,
+    #[arg(short = 'i', long, help = "run login shell as the target user; a command may also be specified", action, conflicts_with("shell"))]
+    pub login: bool,
+    #[arg(short = 'K', long = "remove-timestamp", help = "remove timestamp file completely", action, conflicts_with("reset_timestamp"), conflicts_with("version"))]
+    pub remove_timestamp: bool,
+    #[arg(short = 'k', long = "reset-timestamp", help = "invalidate timestamp file", action, conflicts_with("remove_timestamp"), conflicts_with("version"))]
+    pub reset_timestamp: bool,
+    #[arg(short, long, help = "list user's privileges or check a specific command; use twice for longer format
+    ", action)]
     pub list: bool,
-    /// Hand-written parser for key-value pairs, e.g. envs. But needs flag.
-    // can we strip hyphens?? -
-    #[arg(long = "[VAR=value]", short = 'x', value_parser = parse_key_val::<String, String>)]
-    defines: Vec<(String, String)>,
-}
-
-// It doesn't matter in which order args are given.
-
-
-/// Parse a key-value pair
-fn parse_key_val<T, U>(s: &str) -> Result<(T, U), Box<dyn Error + Send + Sync + 'static>>
-where
-    T: std::str::FromStr,
-    T::Err: Error + Send + Sync + 'static,
-    U: std::str::FromStr,
-    U::Err: Error + Send + Sync + 'static,
-{
-    let pos = s
-        .find('=')
-        .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{}`", s))?;
-    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
+    #[arg(short = 'n', long = "non-interactive", help = "non-interactive mode, no prompts are used", action)]
+    pub non_interactive: bool,
+    #[arg(short = 'P', long = "preserve-groups", help = "preserve group vector instead of setting to target's", action)]
+    pub preserve_groups: bool,
+    #[arg(short = 'p', long = "prompt=prompt", help = "use the specified password prompt")]
+    pub prompt: Option<String>,
+    #[arg(short = 'R', long = "chroot=directory", help = "change the root directory before running command", value_name = "directory")]
+    pub chroot: Option<PathBuf>,
+    #[arg(short = 'S', long, help = "read password from standard input", action)]
+    pub stdin: bool,
+    #[arg(short = 's', long, help = "run shell as the target user; a command may also be specified", action)]
+    pub shell: bool,
+    #[arg(short = 'T', long = "command-timeout=timeout", help = "terminate command after the specified time limit", value_name = "timeout")]
+    pub command_timeout: Option<String>,
+    #[arg(short = 'U', long = "other-user=user", help = "in list mode, display privileges for user", value_name = "user")]
+    pub other_user: Option<String>,
+    #[arg(short = 'u', long = "user=user", help = "run command (or edit file) as specified user name or ID")]
+    pub user: Option<String>,
+    // #[arg(short = 'V', long = "version", help = "display version information and exit!", action = ArgAction::Version, conflicts_with("host"), conflicts_with("remove_timestamp"), conflicts_with("reset_timestamp"))] 
+    // pub version: bool,
+    #[arg(short = 'v', long, help = "update user's timestamp without running a command", action)]
+    pub validate: bool,
+    #[arg(long = " ", help = "stop processing command line arguments", action)] // long arg should be "--", not allowed. How to pass?
+    pub stop_processing_args: bool,
 }
--- sudo/src/main.rs
+++ sudo/src/main.rs
@@ -1,28 +1,13 @@
 mod cli_args;
-use clap::{Parser, CommandFactory};
+use clap::Parser;
 use cli_args::Cli;
+
 
 #[derive(Debug)]
 struct CustomError(String);
 
-// fn main() {
 fn main() -> Result<(), CustomError> {
-
     let args = Cli::parse();
-    // let content = std::fs::read_to_string(&args.path);
-    // if let Some(content) = args.path.as_deref() {
-    if let Some(cli_path) = args.path.as_deref() {
-        let content = cli_path.display();
-        println!("path: {}", content);
-        println!("Value for content: {:?}", std::fs::read_to_string(content.to_string()));
-    }
-
     println!("args: {:?}", args);
     Ok(())
-}
-
-#[test]
-fn verify_cli() {
-    use clap::CommandFactory;
-    Cli::command().debug_assert()
 }
