9f75d8fdffb8285027142965584bcc48447da18f
Marlon <marlonbaeten@users.noreply.github.com>
2023-02-07 11:34:24 +0100

    Cleanup of "sudo light" main (#31)
    * Cleanup of sudo light main
    
    * Update before installing deps on ci
    
    * Run dep install with sudo ;-)
    
    * Fix future clippy warnings

--- lib/sudo-common/src/error.rs
+++ lib/sudo-common/src/error.rs
@@ -4,10 +4,15 @@ pub enum Error {
     UserNotFound,
     Exec,
     Authentication(String),
+    Configuration(String),
 }
 
 impl Error {
     pub fn auth(message: &str) -> Self {
         Self::Authentication(message.to_string())
+    }
+
+    pub fn conf(message: &str) -> Self {
+        Self::Configuration(message.to_string())
     }
 }
