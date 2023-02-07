802902972e0dfc6bdea14c84e01b49a63e5eadc9
Marlon Baeten <marlon@tweedegolf.com>
2023-02-07 12:28:07 +0100

    Added env keep / check tables

--- lib/sudo-common/src/env.rs
+++ lib/sudo-common/src/env.rs
@@ -1,15 +1,33 @@
 use crate::context::{CommandAndArguments, Context};
-use std::{
-    collections::HashMap,
-    env,
-    ffi::{OsStr, OsString},
-};
+use std::{collections::HashMap, env};
 
-pub type Environment = HashMap<OsString, OsString>;
+pub type Environment = HashMap<String, String>;
 
-pub fn environment_from_list<K: Into<OsString>, V: Into<OsString>>(
-    list: Vec<(K, V)>,
-) -> Environment {
+const CHECK_ENV_TABLE: &[&str] = &[
+    "COLORTERM",
+    "LANG",
+    "LANGUAGE",
+    "LC_*",
+    "LINGUAS",
+    "TERM",
+    "TZ",
+];
+
+const KEEP_ENV_TABLE: &[&str] = &[
+    "COLORS",
+    "DISPLAY",
+    "HOSTNAME",
+    "KRB5CCNAME",
+    "LS_COLORS",
+    "PATH",
+    "PS1",
+    "PS2",
+    "XAUTHORITY",
+    "XAUTHORIZATION",
+    "XDG_CURRENT_DESKTOP",
+];
+
+pub fn environment_from_list<K: Into<String>, V: Into<String>>(list: Vec<(K, V)>) -> Environment {
     list.into_iter()
         .map(|(k, v)| (k.into(), v.into()))
         .collect::<Environment>()
@@ -38,49 +56,58 @@ fn format_command(command_and_arguments: &CommandAndArguments) -> String {
 fn get_extra_env(context: &Context) -> Environment {
     let mut extra_env = environment_from_list(vec![
         ("SUDO_COMMAND", format_command(&context.command)),
-        ("SUDO_UID", context.current_user.uid.to_string()),
-        ("SUDO_GID", context.current_user.gid.to_string()),
-        ("SUDO_USER", context.current_user.name.to_string()),
+        ("SUDO_UID", context.target_user.uid.to_string()),
+        ("SUDO_GID", context.target_user.gid.to_string()),
+        ("SUDO_USER", context.target_user.name.clone()),
+        ("SHELL", context.target_user.shell.clone()),
+        // TODO: Set to the login name of the target user when the -i option is specified,
+        // when the set_logname option is enabled in sudoers, or when the env_reset option
+        // is enabled in sudoers (unless LOGNAME is present in the env_keep list).
+        ("LOGNAME", context.target_user.name.clone()),
+        ("USER", context.target_user.name.clone()),
     ]);
 
     if context.set_home {
-        let home: &OsStr = OsStr::new(&context.target_user.home);
-        extra_env.insert("HOME".into(), home.into());
+        extra_env.insert("HOME".to_string(), context.target_user.home.clone());
     }
 
     extra_env
 }
 
-fn filter_env(preserve_env_list: Vec<&str>, environment: Environment) -> Environment {
-    let mut filtered_env = environment;
-    filtered_env.retain(|k, _| {
-        if let Some(name) = k.to_str() {
-            preserve_env_list.contains(&name)
-        } else {
-            false
+fn filter_env(preserve_env_list: Vec<&str>, environment: &Environment) -> Environment {
+    let mut filtered_env = Environment::new();
+
+    for name_pattern in preserve_env_list {
+        if let Ok(pattern) = glob::Pattern::new(name_pattern) {
+            for (name, value) in environment {
+                if pattern.matches(name) {
+                    filtered_env.insert(name.to_string(), value.to_string());
+                }
+            }
         }
-    });
+    }
 
     filtered_env
 }
 
-/// TODO: implement correct behaviour - see https://github.com/sudo-project/sudo/blob/main/plugins/sudoers/env.c
-/// TODO: Add test unit + functional
 pub fn get_target_environment(context: &Context) -> Environment {
     let mut result = Environment::new();
-    let current = env::vars_os().collect::<Environment>();
+    let current = env::vars().collect::<Environment>();
 
-    if context.preserve_env {
-        result.extend(current);
-    } else if context.preserve_env_list.is_empty() {
+    if !context.preserve_env_list.is_empty() {
         let preserve_env_list = context
             .preserve_env_list
             .iter()
             .map(|s| s.as_ref())
             .collect();
 
-        let filtered_current_env = filter_env(preserve_env_list, current);
-        result.extend(filtered_current_env);
+        result.extend(filter_env(preserve_env_list, &current));
+    } else if context.preserve_env {
+        result.extend(current);
+    } else {
+        // TODO filter CHECK_ENV_TABLE list
+        result.extend(filter_env(CHECK_ENV_TABLE.to_vec(), &current));
+        result.extend(filter_env(KEEP_ENV_TABLE.to_vec(), &current));
     }
 
     let sudo_env = get_extra_env(context);
