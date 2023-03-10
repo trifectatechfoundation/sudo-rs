4d5337967e4c33933012e98d256a99f4f381b598
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-03-10 20:39:01 +0100

    add more auth as user tests

--- test-framework/sudo-compliance-tests/src/lib.rs
+++ test-framework/sudo-compliance-tests/src/lib.rs
@@ -96,6 +96,32 @@ fn can_sudo_as_user_if_users_group_is_in_sudoers_file_and_correct_password_is_pr
 }
 
 #[test]
+fn cannot_sudo_as_user_if_users_group_is_in_sudoers_file_and_incorrect_password_is_provided(
+) -> Result<()> {
+    let username = "ferris";
+    let groupname = "rustaceans";
+    let env = EnvBuilder::default()
+        .sudoers(&format!("%{groupname}    ALL=(ALL:ALL) ALL"))
+        .user(username, &[groupname])
+        .user_password(username, "strong-password")
+        .build()?;
+
+    let output = env.exec(
+        &["sudo", "-S", "true"],
+        As::User { name: username },
+        Some("incorrect-password"),
+    )?;
+    assert!(!output.status.success());
+    assert_eq!(Some(1), output.status.code());
+
+    if sudo_test::is_original_sudo() {
+        assert_contains!(output.stderr, "incorrect password attempt");
+    }
+
+    Ok(())
+}
+
+#[test]
 fn cannot_sudo_as_user_if_users_group_is_in_sudoers_file_and_password_is_not_provided() -> Result<()>
 {
     let username = "ferris";
@@ -104,6 +130,71 @@ fn cannot_sudo_as_user_if_users_group_is_in_sudoers_file_and_password_is_not_pro
     let env = EnvBuilder::default()
         .sudoers(&format!("%{groupname}    ALL=(ALL:ALL) ALL"))
         .user(username, &[groupname])
+        .user_password(username, password)
+        .build()?;
+
+    let output = env.exec(&["sudo", "-S", "true"], As::User { name: username }, None)?;
+    assert_eq!(Some(1), output.status.code());
+
+    if sudo_test::is_original_sudo() {
+        assert_contains!(output.stderr, "no password was provided");
+    }
+
+    Ok(())
+}
+
+#[test]
+fn can_sudo_as_user_if_user_is_in_sudoers_file_and_correct_password_is_provided() -> Result<()> {
+    let username = "ferris";
+    let password = "strong-password";
+    let env = EnvBuilder::default()
+        .sudoers(&format!("{username}    ALL=(ALL:ALL) ALL"))
+        .user(username, &["rustaceans"])
+        .user_password(username, password)
+        .build()?;
+
+    let output = env.exec(
+        &["sudo", "-S", "true"],
+        As::User { name: username },
+        Some(password),
+    )?;
+    assert!(output.status.success(), "{}", output.stderr);
+
+    Ok(())
+}
+
+#[test]
+fn cannot_sudo_as_user_if_user_is_in_sudoers_file_and_incorrect_password_is_provided() -> Result<()>
+{
+    let username = "ferris";
+    let env = EnvBuilder::default()
+        .sudoers(&format!("{username}    ALL=(ALL:ALL) ALL"))
+        .user(username, &["rustaceans"])
+        .user_password(username, "strong-password")
+        .build()?;
+
+    let output = env.exec(
+        &["sudo", "-S", "true"],
+        As::User { name: username },
+        Some("incorrect-password"),
+    )?;
+    assert!(!output.status.success());
+    assert_eq!(Some(1), output.status.code());
+
+    if sudo_test::is_original_sudo() {
+        assert_contains!(output.stderr, "incorrect password attempt");
+    }
+
+    Ok(())
+}
+
+#[test]
+fn cannot_sudo_as_user_if_user_is_in_sudoers_file_and_password_is_not_provided() -> Result<()> {
+    let username = "ferris";
+    let password = "strong-password";
+    let env = EnvBuilder::default()
+        .sudoers(&format!("{username}    ALL=(ALL:ALL) ALL"))
+        .user(username, &["rustaceans"])
         .user_password(username, password)
         .build()?;
 
