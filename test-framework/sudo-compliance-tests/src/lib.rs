1f8499d22adfd6e4165868021369c6cada2a3d6f
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-03-10 20:51:30 +0100

    NOPASSWD tests

--- test-framework/sudo-compliance-tests/src/lib.rs
+++ test-framework/sudo-compliance-tests/src/lib.rs
@@ -62,9 +62,21 @@ fn cannot_sudo_if_sudoers_file_is_world_writable() -> Result<()> {
 // man sudoers > User Authentication:
 // "A password is not required if the invoking user is root"
 #[test]
-fn can_sudo_as_root_without_providing_a_password_if_root_is_in_sudoers_file() -> Result<()> {
+fn can_sudo_as_root_without_providing_a_password_if_root_user_is_in_sudoers_file() -> Result<()> {
     let env = EnvBuilder::default()
         .sudoers("root    ALL=(ALL:ALL) ALL")
+        .build()?;
+
+    let output = env.exec(&["sudo", "true"], As::Root, None)?;
+    assert!(output.status.success(), "{}", output.stderr);
+
+    Ok(())
+}
+
+#[test]
+fn can_sudo_as_root_without_providing_a_password_if_roots_group_is_in_sudoers_file() -> Result<()> {
+    let env = EnvBuilder::default()
+        .sudoers("%root    ALL=(ALL:ALL) ALL")
         .build()?;
 
     let output = env.exec(&["sudo", "true"], As::Root, None)?;
@@ -204,6 +216,37 @@ fn cannot_sudo_as_user_if_user_is_in_sudoers_file_and_password_is_not_provided()
     if sudo_test::is_original_sudo() {
         assert_contains!(output.stderr, "no password was provided");
     }
+
+    Ok(())
+}
+
+#[test]
+fn can_sudo_as_user_without_providing_a_password_if_users_group_is_in_sudoers_file_and_nopasswd_is_set(
+) -> Result<()> {
+    let username = "ferris";
+    let groupname = "rustaceans";
+    let env = EnvBuilder::default()
+        .sudoers(&format!("%{groupname}    ALL=(ALL:ALL) NOPASSWD: ALL"))
+        .user(username, &[groupname])
+        .build()?;
+
+    let output = env.exec(&["sudo", "-S", "true"], As::User { name: username }, None)?;
+    assert!(output.status.success(), "{}", output.stderr);
+
+    Ok(())
+}
+
+#[test]
+fn can_sudo_as_user_without_providing_a_password_if_user_is_in_sudoers_file_and_nopasswd_is_set(
+) -> Result<()> {
+    let username = "ferris";
+    let env = EnvBuilder::default()
+        .sudoers(&format!("{username}    ALL=(ALL:ALL) NOPASSWD: ALL"))
+        .user(username, &["rustaceans"])
+        .build()?;
+
+    let output = env.exec(&["sudo", "-S", "true"], As::User { name: username }, None)?;
+    assert!(output.status.success(), "{}", output.stderr);
 
     Ok(())
 }
