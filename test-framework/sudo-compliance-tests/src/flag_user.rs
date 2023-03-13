62329d79723ff9154124231f689da7f513b8a9e3
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-03-13 21:02:59 +0100

    test -u with # syntax

--- test-framework/sudo-compliance-tests/src/flag_user.rs
+++ test-framework/sudo-compliance-tests/src/flag_user.rs
@@ -5,14 +5,35 @@ use crate::{Result, SUDOERS_FERRIS_ALL_NOPASSWD, SUDOERS_ROOT_ALL};
 
 #[ignore]
 #[test]
-fn root_can_become_another_user() -> Result<()> {
+fn root_can_become_another_user_by_name() -> Result<()> {
+    let username = "ferris";
     let env = EnvBuilder::default()
-        .user("ferris", &[])
+        .user(username, &[])
         .sudoers(SUDOERS_ROOT_ALL)
         .build()?;
 
-    let expected = env.stdout(&["id"], As::User { name: "ferris" }, None)?;
-    let actual = env.stdout(&["sudo", "-u", "ferris", "id"], As::Root, None)?;
+    let expected = env.stdout(&["id"], As::User { name: username }, None)?;
+    let actual = env.stdout(&["sudo", "-u", username, "id"], As::Root, None)?;
+
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
+
+#[ignore]
+#[test]
+fn root_can_become_another_user_by_uid() -> Result<()> {
+    let username = "ferris";
+    let env = EnvBuilder::default()
+        .user(username, &[])
+        .sudoers(SUDOERS_ROOT_ALL)
+        .build()?;
+
+    let uid = env
+        .stdout(&["id", "-u"], As::User { name: username }, None)?
+        .parse::<u32>()?;
+    let expected = env.stdout(&["id"], As::User { name: username }, None)?;
+    let actual = env.stdout(&["sudo", "-u", &format!("#{uid}"), "id"], As::Root, None)?;
 
     assert_eq!(expected, actual);
 
