3e6925da41b1ae85042f05c173147641c7f1817a
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-03-13 20:56:16 +0100

    sudoers: user specs evaluated bottom to top

--- test-framework/sudo-compliance-tests/src/sudoers.rs
+++ test-framework/sudo-compliance-tests/src/sudoers.rs
@@ -29,3 +29,33 @@ fn cannot_sudo_if_sudoers_file_is_world_writable() -> Result<()> {
 
     Ok(())
 }
+
+#[ignore]
+#[test]
+fn user_specifications_evaluated_bottom_to_top() -> Result<()> {
+    let username = "ferris";
+    let password = "strong-password";
+    let env = EnvBuilder::default()
+        .user(username, &[])
+        .user_password(username, password)
+        .sudoers("ferris ALL=(ALL:ALL) NOPASSWD: ALL")
+        // overrides the preceding NOPASSWD
+        .sudoers("ferris ALL=(ALL:ALL) ALL")
+        .build()?;
+
+    let output = env.exec(&["sudo", "-S", "true"], As::User { name: username }, None)?;
+    assert!(!output.status.success());
+
+    if sudo_test::is_original_sudo() {
+        assert_contains!(output.stderr, "no password was provided");
+    }
+
+    let output = env.exec(
+        &["sudo", "-S", "true"],
+        As::User { name: username },
+        Some(password),
+    )?;
+    assert!(output.status.success(), "{}", output.stderr);
+
+    Ok(())
+}
