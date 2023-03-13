d51b447a48f280f41e32ab1b48e2444bbcf0c2e6
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-03-13 19:16:14 +0100

    add -u tests

--- test-framework/sudo-compliance-tests/src/lib.rs
+++ test-framework/sudo-compliance-tests/src/lib.rs
@@ -449,3 +449,66 @@ exit 0"#,
 
     Ok(())
 }
+
+#[ignore]
+#[test]
+fn root_can_become_another_user() -> Result<()> {
+    let env = EnvBuilder::default()
+        .user("ferris", &[])
+        .sudoers(SUDOERS_ROOT_FULL_PERMS)
+        .build()?;
+
+    let expected = env.stdout(&["id"], As::User { name: "ferris" }, None)?;
+    let actual = env.stdout(&["sudo", "-u", "ferris", "id"], As::Root, None)?;
+
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
+
+#[ignore]
+#[test]
+fn user_can_become_another_user() -> Result<()> {
+    let env = EnvBuilder::default()
+        .user("ferris", &[])
+        .user("someone_else", &[])
+        .sudoers(FERRIS_NOPASSWD_PERMS)
+        .build()?;
+
+    let expected = env.stdout(
+        &["id"],
+        As::User {
+            name: "someone_else",
+        },
+        None,
+    )?;
+    let actual = env.stdout(
+        &["sudo", "-u", "someone_else", "id"],
+        As::User { name: "ferris" },
+        None,
+    )?;
+
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
+
+#[ignore]
+#[test]
+fn user_can_become_themselves_without_providing_a_password() -> Result<()> {
+    let username = "ferris";
+    let env = EnvBuilder::default()
+        .user(username, &[])
+        .sudoers(&format!("{username}    ALL=(ALL:ALL) ALL"))
+        .build()?;
+
+    let output = env.exec(
+        &["sudo", "-u", username, "true"],
+        As::User { name: username },
+        None,
+    )?;
+
+    assert!(output.status.success(), "{}", output.stderr);
+
+    Ok(())
+}
