e78935131f0c3c3674fc05f49245af9c424c76c7
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-03-15 20:06:32 +0100

    add regression test for #81

--- test-framework/sudo-compliance-tests/src/flag_user.rs
+++ test-framework/sudo-compliance-tests/src/flag_user.rs
@@ -1,5 +1,5 @@
 use pretty_assertions::assert_eq;
-use sudo_test::{Command, Env};
+use sudo_test::{Command, Env, User};
 
 use crate::{Result, SUDOERS_FERRIS_ALL_NOPASSWD, SUDOERS_ROOT_ALL_NOPASSWD};
 
@@ -43,11 +43,37 @@ fn root_can_become_another_user_by_uid() -> Result<()> {
     Ok(())
 }
 
-#[ignore]
 #[test]
 fn user_can_become_another_user() -> Result<()> {
     let env = Env(SUDOERS_FERRIS_ALL_NOPASSWD)
         .user("ferris")
+        .user("someone_else")
+        .build()?;
+
+    let expected = Command::new("id")
+        .as_user("someone_else")
+        .exec(&env)?
+        .stdout()?;
+    let actual = Command::new("sudo")
+        .args(["-u", "someone_else", "id"])
+        .as_user("ferris")
+        .exec(&env)?
+        .stdout()?;
+
+    assert_eq!(expected, actual);
+
+    Ok(())
+}
+
+// regression test for memorysafety/sudo-rs#81
+#[test]
+#[ignore]
+fn invoking_user_groups_are_lost_when_becoming_another_user() -> Result<()> {
+    let groupname = "rustaceans";
+
+    let env = Env(SUDOERS_FERRIS_ALL_NOPASSWD)
+        .group(groupname)
+        .user(User("ferris").group(groupname))
         .user("someone_else")
         .build()?;
 
