8c28249c1215941dafb90eeca2729af8bb4ba7f0
Christian Poveda <christian.poveda@ferrous-systems.com>
2023-04-05 23:03:57 -0500

    Use `insta` for the `flag_user.rs` tests

--- test-framework/sudo-compliance-tests/src/flag_user.rs
+++ test-framework/sudo-compliance-tests/src/flag_user.rs
@@ -111,7 +111,7 @@ fn unassigned_user_id_is_rejected() -> Result<()> {
         assert_eq!(Some(1), output.status().code());
 
         if sudo_test::is_original_sudo() {
-            assert_contains!(output.stderr(), "sudo: unknown user: #1234");
+            insta::assert_snapshot!(output.stderr());
         }
     }
 
