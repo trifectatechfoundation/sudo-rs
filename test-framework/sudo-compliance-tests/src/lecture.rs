1f980b6552d04a22c7b2126ea7352b8580649483
Brigitte Markmann <brigitte.markmann@ferrous-systems.com>
2023-05-22 15:50:17 +0200

    Adress review.

--- test-framework/sudo-compliance-tests/src/lecture.rs
+++ test-framework/sudo-compliance-tests/src/lecture.rs
@@ -1,16 +1,10 @@
 use sudo_test::{Command, Env, User};
-use crate::{Result, SUDOERS_ROOT_ALL, USERNAME, SUDOERS_USER_ALL_ALL, SUDOERS_ALWAYS_LECTURE, SUDOERS_NO_LECTURE, PASSWORD};
+use crate::{Result, SUDOERS_ROOT_ALL, USERNAME, SUDOERS_USER_ALL_ALL, SUDOERS_ALWAYS_LECTURE, SUDOERS_NO_LECTURE, PASSWORD, OG_SUDO_STANDARD_LECTURE};
 
 #[test]
+#[ignore]
 fn default_lecture_shown_once() -> Result<()> {
-    let expected_lecture = format!(
-        "\nWe trust you have received the usual lecture from the local System\nAdministrator. It usually boils down to these three things:\n\n    #1) Respect the privacy of others.\n    #2) Think before you type.\n    #3) With great power comes great responsibility."
-    );
-    let expected_echo = format!(
-        "Yeah!"
-    );
-
-    let env = Env(["SUDOERS_ROOT_ALL", SUDOERS_USER_ALL_ALL])
+    let env = Env([SUDOERS_ROOT_ALL, SUDOERS_USER_ALL_ALL])
         .user(User(USERNAME).password(PASSWORD))
         .build()?;
 
@@ -21,12 +15,10 @@ fn default_lecture_shown_once() -> Result<()> {
     .exec(&env)?;
     assert_eq!(true, output.status().success());
 
-    if sudo_test::is_original_sudo() {
-        assert_contains!(
-            output.stderr(),
-            expected_lecture
-        );
-    }
+    assert_contains!(
+        output.stderr(),
+        OG_SUDO_STANDARD_LECTURE
+    );
 
     let second_sudo = Command::new("sudo")
     .as_user(USERNAME)
@@ -35,22 +27,17 @@ fn default_lecture_shown_once() -> Result<()> {
     .exec(&env)?;
 
     assert_eq!(true, second_sudo.status().success());
-    assert_eq!(Some(0), second_sudo.status().code());
-    assert_eq!(second_sudo.stdout().unwrap(), expected_echo);
+    assert_ne!(second_sudo.stdout().unwrap(), OG_SUDO_STANDARD_LECTURE);
     Ok(())
 }
 
 #[test]
+#[ignore]
 fn lecture_always_shown() -> Result<()> {
-    let expected_lecture = format!(
-        "\nWe trust you have received the usual lecture from the local System\nAdministrator. It usually boils down to these three things:\n\n    #1) Respect the privacy of others.\n    #2) Think before you type.\n    #3) With great power comes great responsibility."
-    );
-    // When implemented, switch lecture and check for the new one
-    // (so test can fail for sudo-rs).
     let env = Env([
         SUDOERS_ROOT_ALL,
-        SUDOERS_ALWAYS_LECTURE,
-        "timestamp_timeout=0"])
+        SUDOERS_ALWAYS_LECTURE
+        ])
         .user(USERNAME).build()?;
 
     let output = Command::new("sudo")
@@ -58,51 +45,41 @@ fn lecture_always_shown() -> Result<()> {
     .stdin(PASSWORD)
     .args(["-S", "true"])
     .exec(&env)?;
-    assert_eq!(Some(1), output.status().code());
     assert_eq!(false, output.status().success());
 
-    if sudo_test::is_original_sudo() {
-        assert_contains!(
-            output.stderr(),
-            expected_lecture
-        );
-    }
+    assert_contains!(
+        output.stderr(),
+        OG_SUDO_STANDARD_LECTURE
+    );
 
     let second_sudo = Command::new("sudo")
     .as_user(USERNAME)
     .stdin(PASSWORD)
     .args(["-S", "ls"])
     .exec(&env)?;
-    assert_eq!(Some(1), output.status().code());
     assert_eq!(false, output.status().success());
 
-    if sudo_test::is_original_sudo() {
-        assert_contains!(
-            second_sudo.stderr(),
-            expected_lecture
-        );
-    }
+    assert_contains!(
+        second_sudo.stderr(),
+        OG_SUDO_STANDARD_LECTURE
+    );
+    // to do, add sudo-rs lecture
     Ok(())
 }
 
 #[test]
 fn lecture_never_shown() -> Result<()> {
-    let expected_echo = format!(
-        "Yeah!"
-    );
-
     let env = Env([SUDOERS_ROOT_ALL, SUDOERS_USER_ALL_ALL, SUDOERS_NO_LECTURE])
         .user(User(USERNAME).password(PASSWORD))
         .build()?;
 
     let output = Command::new("sudo")
-    .args(["-S", "echo", "Yeah!"])
     .as_user(USERNAME)
     .stdin(PASSWORD)
+    .args(["-S", "echo", "Yeah!"])
     .exec(&env)?;
 
     assert_eq!(true, output.status().success());
-    assert_eq!(Some(0), output.status().code());
-    assert_eq!(output.stdout().unwrap(), expected_echo);
+    assert_ne!(output.stdout().unwrap(), OG_SUDO_STANDARD_LECTURE);
     Ok(())
 }
--- test-framework/sudo-compliance-tests/src/lecture_file.rs
+++ test-framework/sudo-compliance-tests/src/lecture_file.rs
@@ -1,43 +1,43 @@
-use sudo_test::{Command, Env};
-use crate::{Result, SUDOERS_ROOT_ALL, USERNAME, SUDOERS_NEW_LECTURE, SUDOERS_NEW_LECTURE_USER};
+use sudo_test::{Command, Env, User};
+use crate::{Result, SUDOERS_ROOT_ALL, USERNAME, PASSWORD, SUDOERS_NEW_LECTURE, SUDOERS_NEW_LECTURE_USER, OG_SUDO_STANDARD_LECTURE};
 
+#[ignore]
 #[test]
 fn default_lecture_message() -> Result<()> {
-    let expected = format!(
-        "\nWe trust you have received the usual lecture from the local System\nAdministrator. It usually boils down to these three things:\n\n    #1) Respect the privacy of others.\n    #2) Think before you type.\n    #3) With great power comes great responsibility."
-    );
-    let env = Env(SUDOERS_ROOT_ALL).user(USERNAME).build()?;
+    let env = Env(SUDOERS_ROOT_ALL)
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
 
     let output = Command::new("sudo")
-    .arg(format!("ls"))
+    .args(["-S", "true"])
     .as_user(USERNAME)
+    .stdin(PASSWORD)
     .exec(&env)?;
-    assert_eq!(Some(1), output.status().code());
-    assert_eq!(false, output.status().success());
-    if sudo_test::is_original_sudo() {
-        assert_contains!(
-            output.stderr(),
-            expected
-        );
-    }
+
+    assert_contains!(
+        output.stderr(),
+        OG_SUDO_STANDARD_LECTURE
+    );
+
     Ok(())
 }
 
-#[test]
 #[ignore]
+#[test]
 fn new_lecture_message() -> Result<()> {
     let new_lecture = format!("I <3 sudo");
     let env = Env([
         SUDOERS_ROOT_ALL,
         SUDOERS_NEW_LECTURE])
         .file("/etc/sudo_lecture", new_lecture)
-        .user(USERNAME).build()?;
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
 
     let output = Command::new("sudo")
     .as_user(USERNAME)
-    .arg(format!("ls"))
+    .stdin(PASSWORD)
+    .args(["-S", "true"])
     .exec(&env)?;
-    assert_eq!(Some(1), output.status().code());
     assert_eq!(false, output.status().success());
     assert_contains!(
         output.stderr(),
@@ -54,13 +54,14 @@ fn new_lecture_for_specific_user() -> Result<()> {
         SUDOERS_ROOT_ALL,
         SUDOERS_NEW_LECTURE_USER])
         .file("/etc/sudo_lecture", new_lecture)
-        .user(USERNAME).build()?;
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
 
     let output = Command::new("sudo")
     .as_user(USERNAME)
-    .arg(format!("ls"))
+    .stdin(PASSWORD)
+    .args(["-S", "true"])
     .exec(&env)?;
-    assert_eq!(Some(1), output.status().code());
     assert_eq!(false, output.status().success());
     assert_contains!(
         output.stderr(),
@@ -69,29 +70,27 @@ fn new_lecture_for_specific_user() -> Result<()> {
     Ok(())
 }
 
+#[ignore]
 #[test]
 fn default_lecture_for_unspecified_user() -> Result<()> {
-    let expected = format!(
-        "\nWe trust you have received the usual lecture from the local System\nAdministrator. It usually boils down to these three things:\n\n    #1) Respect the privacy of others.\n    #2) Think before you type.\n    #3) With great power comes great responsibility."
-    );
     let new_lecture = format!("I <3 sudo");
     let env = Env([
         SUDOERS_ROOT_ALL,
         SUDOERS_NEW_LECTURE_USER])
         .file("/etc/sudo_lecture", new_lecture)
-        .user(USERNAME).user("other_user").build()?;
+        .user(User(USERNAME).password(PASSWORD))
+        .user(User("other_user").password("other_password"))
+        .build()?;
 
     let output = Command::new("sudo")
     .as_user("other_user")
-    .arg(format!("ls"))
+    .stdin("other_password")
+    .args(["-S", "true"])
     .exec(&env)?;
-    assert_eq!(Some(1), output.status().code());
     assert_eq!(false, output.status().success());
-    if sudo_test::is_original_sudo() {
-        assert_contains!(
-            output.stderr(),
-            expected
-        );
-    }
+    assert_contains!(
+        output.stderr(),
+        OG_SUDO_STANDARD_LECTURE
+    );
     Ok(())
 }
