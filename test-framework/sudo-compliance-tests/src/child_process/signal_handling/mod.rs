75379fe1d8a076c0e47aa1cbb74b7273bf11b3c4
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-05-22 18:30:30 +0200

    test SIGTSTP handling

--- test-framework/sudo-compliance-tests/src/child_process/signal_handling/mod.rs
+++ test-framework/sudo-compliance-tests/src/child_process/signal_handling/mod.rs
@@ -1,7 +1,7 @@
 use pretty_assertions::assert_eq;
 use sudo_test::{Command, Env};
 
-use crate::{Result, SUDOERS_USER_ALL_NOPASSWD, USERNAME};
+use crate::{Result, SUDOERS_ALL_ALL_NOPASSWD, SUDOERS_USER_ALL_NOPASSWD, USERNAME};
 
 // man sudo > Signal handling
 // "As a special case, sudo will not relay signals that were sent by the command it is running."
@@ -64,6 +64,44 @@ fn child_terminated_by_signal() -> Result<()> {
 
     assert_eq!(Some(143), output.status().code());
     assert!(output.stderr().is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn sigtstp_works() -> Result<()> {
+    const STOP_DELAY: u64 = 5;
+    const NUM_ITERATIONS: usize = 5;
+
+    let script_path = "/tmp/script.sh";
+    let env = Env(SUDOERS_ALL_ALL_NOPASSWD)
+        .file(script_path, include_str!("sigtstp.bash"))
+        .build()?;
+
+    let output = Command::new("bash").arg(script_path).exec(&env)?.stdout()?;
+
+    let timestamps = output
+        .lines()
+        .filter_map(|line| line.parse::<u64>().ok())
+        .collect::<Vec<_>>();
+
+    dbg!(&timestamps);
+
+    assert_eq!(NUM_ITERATIONS, timestamps.len());
+
+    let suspended_iterations = timestamps
+        .windows(2)
+        .filter(|window| {
+            let prev_timestamp = window[0];
+            let curr_timestamp = window[1];
+            let delta = curr_timestamp - prev_timestamp;
+
+            delta >= STOP_DELAY
+        })
+        .count();
+    let did_suspend = suspended_iterations == 1;
+
+    assert!(did_suspend);
 
     Ok(())
 }
