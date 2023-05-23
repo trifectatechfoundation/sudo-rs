975e7f40a94b8b80ecde5570d42b0f1d77f4355c
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-05-23 17:51:06 +0200

    test pass auth with longer passwords

--- test-framework/sudo-compliance-tests/src/pass_auth.rs
+++ test-framework/sudo-compliance-tests/src/pass_auth.rs
@@ -66,3 +66,78 @@ fn no_password() -> Result<()> {
 
     Ok(())
 }
+
+const MAX_PAM_RESPONSE_SIZE: usize = 512;
+
+#[test]
+fn longest_possible_password_works() -> Result<()> {
+    let password = "a".repeat(MAX_PAM_RESPONSE_SIZE - 1 /* null byte */);
+
+    let env = Env("ALL ALL=(ALL:ALL) ALL")
+        .user(User(USERNAME).password(&password))
+        .build()?;
+
+    Command::new("sudo")
+        .args(["-S", "true"])
+        .stdin(password)
+        .as_user(USERNAME)
+        .exec(&env)?
+        .assert_success()
+}
+
+#[test]
+fn input_longer_than_max_pam_response_size_is_handled_gracefully() -> Result<()> {
+    let env = Env("ALL ALL=(ALL:ALL) ALL").user(USERNAME).build()?;
+
+    let input = "a".repeat(5 * MAX_PAM_RESPONSE_SIZE / 2);
+    let output = Command::new("sudo")
+        .args(["-S", "true"])
+        .stdin(input)
+        .as_user(USERNAME)
+        .exec(&env)?;
+
+    assert!(!output.status().success());
+    assert_eq!(Some(1), output.status().code());
+
+    let stderr = output.stderr();
+    if sudo_test::is_original_sudo() {
+        assert_contains!(stderr, "sudo: 2 incorrect password attempts");
+    } else {
+        assert_contains!(stderr, "Authentication failure");
+        assert_not_contains!(stderr, "panic");
+    }
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn input_longer_than_password_should_not_be_accepted_as_correct_password() -> Result<()> {
+    let password = "a".repeat(MAX_PAM_RESPONSE_SIZE - 1 /* null byte */);
+    let env = Env("ALL ALL=(ALL:ALL) ALL")
+        .user(User(USERNAME).password(password))
+        .build()?;
+
+    let input_sizes = [MAX_PAM_RESPONSE_SIZE, MAX_PAM_RESPONSE_SIZE + 1];
+
+    for input_size in input_sizes {
+        let input = "a".repeat(input_size);
+        let output = Command::new("sudo")
+            .args(["-S", "true"])
+            .stdin(input)
+            .as_user(USERNAME)
+            .exec(&env)?;
+
+        assert!(!output.status().success());
+        assert_eq!(Some(1), output.status().code());
+
+        let stderr = output.stderr();
+        if sudo_test::is_original_sudo() {
+            assert_contains!(stderr, "sudo: 1 incorrect password attempt");
+        } else {
+            assert_contains!(stderr, "Authentication failure");
+        }
+    }
+
+    Ok(())
+}
