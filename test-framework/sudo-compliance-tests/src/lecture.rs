022c3e2b0c104586ac36c78c06d226127b68581f
Brigitte Markmann <brigitte.markmann@ferrous-systems.com>
2023-05-26 14:55:15 +0200

    Add tests for NOPASSWD, change to assert_not_contains, check stderr.

--- test-framework/sudo-compliance-tests/src/lecture.rs
+++ test-framework/sudo-compliance-tests/src/lecture.rs
@@ -1,5 +1,5 @@
 use sudo_test::{Command, Env, User};
-use crate::{Result, SUDOERS_ROOT_ALL, USERNAME, SUDOERS_USER_ALL_ALL, SUDOERS_ALWAYS_LECTURE, SUDOERS_NO_LECTURE, PASSWORD, OG_SUDO_STANDARD_LECTURE};
+use crate::{Result, SUDOERS_ROOT_ALL, USERNAME, SUDOERS_USER_ALL_ALL, SUDOERS_ALWAYS_LECTURE, SUDOERS_NO_LECTURE, PASSWORD, OG_SUDO_STANDARD_LECTURE, SUDOERS_USER_ALL_NOPASSWD};
 
 #[test]
 #[ignore]
@@ -27,7 +27,30 @@ fn default_lecture_shown_once() -> Result<()> {
     .exec(&env)?;
 
     assert_eq!(true, second_sudo.status().success());
-    assert_ne!(second_sudo.stdout().unwrap(), OG_SUDO_STANDARD_LECTURE);
+    assert_not_contains!(second_sudo.stderr(), OG_SUDO_STANDARD_LECTURE);
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn lecture_in_stderr() -> Result<()> {
+    let env = Env([SUDOERS_ROOT_ALL, SUDOERS_USER_ALL_ALL])
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
+
+    let output = Command::new("sudo")
+    .args(["-S", "true"])
+    .as_user(USERNAME)
+    .stdin(PASSWORD)
+    .exec(&env)?;
+    assert_eq!(true, output.status().success());
+
+    assert_contains!(
+        output.stderr(),
+        OG_SUDO_STANDARD_LECTURE
+    );
+
+    assert_not_contains!(output.stdout().unwrap(), OG_SUDO_STANDARD_LECTURE);
     Ok(())
 }
 
@@ -79,7 +102,7 @@ fn lecture_never_shown() -> Result<()> {
     .exec(&env)?;
 
     assert_eq!(true, output.status().success());
-    assert_ne!(output.stdout().unwrap(), OG_SUDO_STANDARD_LECTURE);
+    assert_not_contains!(output.stderr(), OG_SUDO_STANDARD_LECTURE);
     Ok(())
 }
 
@@ -96,6 +119,60 @@ fn negation_equals_never() -> Result<()> {
     .exec(&env)?;
 
     assert_eq!(true, output.status().success());
-    assert_ne!(output.stdout().unwrap(), OG_SUDO_STANDARD_LECTURE);
+    assert_not_contains!(output.stderr(), OG_SUDO_STANDARD_LECTURE);
+    Ok(())
+}
+
+#[test]
+fn double_negation_also_equals_never() -> Result<()> {
+    let env = Env([SUDOERS_ROOT_ALL, SUDOERS_USER_ALL_ALL, "Defaults  !!lecture"])
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
+
+    let output = Command::new("sudo")
+    .args(["-S", "true"])
+    .as_user(USERNAME)
+    .stdin(PASSWORD)
+    .exec(&env)?;
+
+    assert_eq!(true, output.status().success());
+    assert_not_contains!(output.stderr(), OG_SUDO_STANDARD_LECTURE);
+
+    Ok(())
+}
+
+/// Lectures are only shown when password is asked for
+#[test]
+fn root_user_lecture_not_shown() -> Result<()> {
+    let env = Env(SUDOERS_ROOT_ALL)
+        .build()?;
+
+    let output = Command::new("sudo")
+    .as_user("root")
+    .stdin(PASSWORD)
+    .args(["-S", "true"])
+    .exec(&env)?;
+
+    assert_eq!(true, output.status().success());
+    assert_not_contains!(output.stderr(), OG_SUDO_STANDARD_LECTURE);
+
+    Ok(())
+}
+
+#[test]
+fn nopasswd_lecture_not_shown() -> Result<()> {
+    let env = Env(SUDOERS_USER_ALL_NOPASSWD)
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
+
+    let output = Command::new("sudo")
+    .as_user(USERNAME)
+    .stdin(PASSWORD)
+    .args(["-S", "true"])
+    .exec(&env)?;
+
+    assert_eq!(true, output.status().success());
+    assert_not_contains!(output.stderr(), OG_SUDO_STANDARD_LECTURE);
+
     Ok(())
 }
