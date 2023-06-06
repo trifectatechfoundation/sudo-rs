d533afb0ae35aba218791e5a48808334a47a8153
Christian Poveda <christian.poveda@ferrous-systems.com>
2023-06-05 20:55:47 -0500

    Use sockets for IPC

---
+++ lib/sudo-exec/src/backchannel.rs
@@ -0,0 +1,158 @@
+use std::{
+    ffi::c_int,
+    io::{self, Read, Write},
+    mem::size_of,
+    os::unix::{net::UnixStream, process::ExitStatusExt},
+    process::ExitStatus,
+};
+
+use sudo_system::interface::ProcessId;
+
+type Prefix = u8;
+type ParentData = c_int;
+const PREFIX_LEN: usize = size_of::<Prefix>();
+const PARENT_DATA_LEN: usize = size_of::<ParentData>();
+
+pub(crate) fn backchannel_pair() -> io::Result<(ParentBackchannel, MonitorBackchannel)> {
+    let (sock1, sock2) = UnixStream::pair()?;
+    sock1.set_nonblocking(true)?;
+    sock2.set_nonblocking(true)?;
+    Ok((
+        ParentBackchannel { socket: sock1 },
+        MonitorBackchannel { socket: sock2 },
+    ))
+}
+
+pub(crate) enum ParentEvent {
+    IoError(c_int),
+    CommandExit(c_int),
+    CommandSignal(c_int),
+    CommandPid(ProcessId),
+}
+
+impl ParentEvent {
+    const LEN: usize = PREFIX_LEN + PARENT_DATA_LEN;
+}
+
+impl<'a> From<&'a io::Error> for ParentEvent {
+    fn from(err: &'a io::Error) -> Self {
+        // This only panics if an error is created using `io::Error::new`.
+        Self::IoError(err.raw_os_error().unwrap())
+    }
+}
+
+impl From<ExitStatus> for ParentEvent {
+    fn from(status: ExitStatus) -> Self {
+        if let Some(code) = status.code() {
+            Self::CommandExit(code)
+        } else {
+            // `ExitStatus::code` docs state that it only returns `None` if the process was
+            // terminated by a signal so this should always succeed.
+            Self::CommandSignal(status.signal().unwrap())
+        }
+    }
+}
+
+/// A socket use for commmunication between the monitor and the parent process.
+pub(crate) struct ParentBackchannel {
+    socket: UnixStream,
+}
+
+impl ParentBackchannel {
+    /// Send a [`MonitorEvent`].
+    ///
+    /// Calling this method will block until the socket is ready for writing.
+    pub(crate) fn send(&mut self, event: &MonitorEvent) -> io::Result<()> {
+        let prefix: Prefix = match event {
+            MonitorEvent::ExecCommand => 0,
+        };
+
+        let buf: [u8; MonitorEvent::LEN] = prefix.to_ne_bytes();
+
+        self.socket.write_all(&buf)
+    }
+
+    /// Receive a [`ParentEvent`].
+    ///
+    /// Calling this method will block until the socket is ready for reading.
+    pub(crate) fn recv(&mut self) -> io::Result<ParentEvent> {
+        let mut buf = [0; ParentEvent::LEN];
+        self.socket.read_exact(&mut buf)?;
+
+        let (prefix_buf, data_buf) = buf.split_at(PREFIX_LEN);
+
+        let prefix = Prefix::from_ne_bytes(prefix_buf.try_into().unwrap());
+        let data = ParentData::from_ne_bytes(data_buf.try_into().unwrap());
+
+        let event = match prefix {
+            0 => ParentEvent::IoError(data),
+            1 => ParentEvent::CommandExit(data),
+            2 => ParentEvent::CommandSignal(data),
+            3 => ParentEvent::CommandPid(data),
+            _ => unreachable!(),
+        };
+
+        Ok(event)
+    }
+}
+
+/// Different messages exchanged between the monitor and the parent process using a [`Backchannel`].
+pub(crate) enum MonitorEvent {
+    ExecCommand,
+}
+
+impl MonitorEvent {
+    const LEN: usize = PREFIX_LEN;
+}
+
+/// A socket use for commmunication between the monitor and the parent process.
+pub(crate) struct MonitorBackchannel {
+    socket: UnixStream,
+}
+
+impl MonitorBackchannel {
+    /// Send a [`ParentEvent`].
+    ///
+    /// Calling this method will block until the socket is ready for writing.
+    pub(crate) fn send(&mut self, event: ParentEvent) -> io::Result<()> {
+        let mut buf = [0; ParentEvent::LEN];
+
+        let (prefix_buf, data_buf) = buf.split_at_mut(PREFIX_LEN);
+
+        let prefix: Prefix = match event {
+            ParentEvent::IoError(_) => 0,
+            ParentEvent::CommandExit(_) => 1,
+            ParentEvent::CommandSignal(_) => 2,
+            ParentEvent::CommandPid(_) => 3,
+        };
+
+        let data: ParentData = match event {
+            ParentEvent::IoError(int)
+            | ParentEvent::CommandExit(int)
+            | ParentEvent::CommandSignal(int)
+            | ParentEvent::CommandPid(int) => int,
+        };
+
+        prefix_buf.copy_from_slice(&prefix.to_ne_bytes());
+        data_buf.copy_from_slice(&data.to_ne_bytes());
+
+        self.socket.write_all(&buf)
+    }
+
+    /// Receive a [`MonitorEvent`].
+    ///
+    /// Calling this method will block until the socket is ready for reading.
+    pub(crate) fn recv(&mut self) -> io::Result<MonitorEvent> {
+        let mut buf = [0; MonitorEvent::LEN];
+        self.socket.read_exact(&mut buf)?;
+
+        let prefix = Prefix::from_ne_bytes(buf);
+
+        let event = match prefix {
+            0 => MonitorEvent::ExecCommand,
+            _ => unreachable!(),
+        };
+
+        Ok(event)
+    }
+}
--- lib/sudo-exec/src/pty.rs
+++ lib/sudo-exec/src/pty.rs
@@ -1,10 +1,15 @@
-use std::{io, os::fd::OwnedFd};
+use std::{io, ops::ControlFlow, os::fd::OwnedFd};
 
 use signal_hook::consts::*;
 use sudo_log::user_error;
 use sudo_system::{getpgid, interface::ProcessId, kill, signal::SignalInfo};
 
-use crate::{signal::SignalHandlers, ExitReason};
+use crate::{
+    backchannel::{MonitorEvent, ParentBackchannel, ParentEvent},
+    io_util::{retry_while_interrupted, was_interrupted},
+    signal::SignalHandlers,
+    ExitReason,
+};
 
 pub(super) struct PtyRelay {
     signal_handlers: SignalHandlers,
@@ -14,7 +19,7 @@ pub(super) struct PtyRelay {
     // FIXME: Look for `SFD_LEADER` occurences in `exec_pty` to decide what to do with the leader
     // side of the pty. It should be used to handle signals like `SIGWINCH` and `SIGCONT`.
     _pty_leader: OwnedFd,
-    rx: OwnedFd,
+    backchannel: ParentBackchannel,
 }
 
 impl PtyRelay {
@@ -22,24 +27,34 @@ impl PtyRelay {
         monitor_pid: ProcessId,
         sudo_pid: ProcessId,
         pty_leader: OwnedFd,
-        rx: OwnedFd,
+        mut backchannel: ParentBackchannel,
     ) -> io::Result<Self> {
+        retry_while_interrupted(|| backchannel.send(&MonitorEvent::ExecCommand))?;
+
         Ok(Self {
             signal_handlers: SignalHandlers::new()?,
             monitor_pid,
             sudo_pid,
-            // FIXME: is this ok? Check ogsudo's code.
             command_pid: -1,
             _pty_leader: pty_leader,
-            rx,
+            backchannel,
         })
     }
 
     pub(super) fn run(mut self) -> io::Result<(ExitReason, impl FnOnce())> {
         loop {
-            // First we check if the monitor sent us the exit status of the command.
-            if let Ok(reason) = self.wait_monitor() {
-                return Ok((reason, move || drop(self.signal_handlers)));
+            // First we check the backchannel for any status updates from the command or the
+            // monitor.
+            if let ControlFlow::Break(event) = self.check_backchannel() {
+                let exit_reason = match event {
+                    ParentEvent::CommandExit(code) => ExitReason::Code(code),
+                    ParentEvent::CommandSignal(signal) => ExitReason::Signal(signal),
+                    ParentEvent::IoError(raw) => return Err(io::Error::from_raw_os_error(raw)),
+                    // We never break the event loop because of this event.
+                    ParentEvent::CommandPid(_) => unreachable!(),
+                };
+
+                return Ok((exit_reason, move || drop(self.signal_handlers)));
             }
 
             // Then we check any pending signals that we received. Based on `signal_cb_pty`
@@ -51,11 +66,33 @@ impl PtyRelay {
         }
     }
 
-    fn wait_monitor(&mut self) -> io::Result<ExitReason> {
-        ExitReason::recv(&self.rx)
+    /// Read an event from the backchannel and return the event if it should break the event loop.
+    fn check_backchannel(&mut self) -> ControlFlow<ParentEvent> {
+        match self.backchannel.recv() {
+            // Not an actual error, we can retry later.
+            Err(err) if was_interrupted(&err) => {}
+            // Failed to read command status. This means that something is wrong with the socket
+            // and we should stop.
+            Err(err) => {
+                return ControlFlow::Break((&err).into());
+            }
+            Ok(event) => match event {
+                // Received the PID of the command. This means that the command is already
+                // executing.
+                ParentEvent::CommandPid(pid) => self.command_pid = pid,
+                // The command terminated or the monitor was not able to spawn it. We should stop
+                // either way.
+                ParentEvent::CommandExit(_)
+                | ParentEvent::CommandSignal(_)
+                | ParentEvent::IoError(_) => {
+                    return ControlFlow::Break(event);
+                }
+            },
+        }
+        ControlFlow::Continue(())
     }
 
-    fn relay_signal(&self, info: SignalInfo) {
+    fn relay_signal(&mut self, info: SignalInfo) {
         match info.signal() {
             // FIXME: check `handle_sigchld_pty`
             SIGCHLD => {}
