91b7c2eeaf5445abd1053339ab05ddd43c37742a
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-06-08 14:13:22 +0200

    add a few more --reset-timestamp tests

--- test-framework/sudo-compliance-tests/src/timestamp/reset.rs
+++ test-framework/sudo-compliance-tests/src/timestamp/reset.rs
@@ -82,7 +82,7 @@ fn with_command_prompts_for_password() -> Result<()> {
 }
 
 #[test]
-fn with_command_does_not_invalidate_credential_cache() -> Result<()> {
+fn with_command_failure_does_not_invalidate_credential_cache() -> Result<()> {
     let env = Env(format!("{USERNAME} ALL=(ALL:ALL) ALL"))
         .user(User(USERNAME).password(PASSWORD))
         .build()?;
@@ -105,4 +105,53 @@ fn with_command_does_not_invalidate_credential_cache() -> Result<()> {
         .as_user(USERNAME)
         .exec(&env)?
         .assert_success()
+}
+
+#[test]
+fn with_command_success_does_not_invalidate_credential_cache() -> Result<()> {
+    let env = Env(format!("{USERNAME} ALL=(ALL:ALL) ALL"))
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
+
+    // the first command, `sudo -S true`, succeeds and caches credentials
+    //
+    // the second command, `sudo -k true`, succeeds
+    //
+    // the last command, `sudo true`, is expected to work because `sudo -k true` did *not*
+    // invalidate the credentials
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "echo {PASSWORD} | sudo -S true; echo {PASSWORD} | sudo -k -S true; sudo true"
+        ))
+        .as_user(USERNAME)
+        .exec(&env)?
+        .assert_success()
+}
+
+#[test]
+fn with_command_does_not_cache_credentials() -> Result<()> {
+    let env = Env(format!("{USERNAME} ALL=(ALL:ALL) ALL"))
+        .user(User(USERNAME).password(PASSWORD))
+        .build()?;
+
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "echo {PASSWORD} | sudo -k -S true 2>/dev/null || exit 2; sudo true"
+        ))
+        .as_user(USERNAME)
+        .exec(&env)?;
+
+    assert!(!output.status().success());
+    assert_eq!(Some(1), output.status().code());
+
+    let diagnostic = if sudo_test::is_original_sudo() {
+        "a password is required"
+    } else {
+        "Authentication failed"
+    };
+    assert_contains!(output.stderr(), diagnostic);
+
+    Ok(())
 }
