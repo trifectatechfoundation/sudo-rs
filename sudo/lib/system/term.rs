5d0ccf3f3c4022bbace315f04a28561ac8440dde
Christian Poveda <christian.poveda@ferrous-systems.com>
2023-06-13 15:53:58 -0500

    Get the path of the pty

--- sudo/lib/system/term.rs
+++ sudo/lib/system/term.rs
@@ -1,4 +1,5 @@
 use std::{
+    ffi::{c_uchar, CString},
     io,
     os::fd::{AsRawFd, FromRawFd, OwnedFd},
     ptr::null_mut,
@@ -9,24 +10,44 @@ use crate::cutils::cerr;
 use super::interface::ProcessId;
 
 pub struct Pty {
+    pub path: CString,
     pub leader: OwnedFd,
     pub follower: OwnedFd,
 }
 
 impl Pty {
     pub fn open() -> io::Result<Self> {
+        const PATH_MAX: usize = libc::PATH_MAX as _;
+        // Allocate a buffer to hold the path to the pty.
+        let mut path = vec![0 as c_uchar; PATH_MAX];
+        // Create two integers to hold the file descriptors for each side of the pty.
         let (mut leader, mut follower) = (0, 0);
+
         cerr(unsafe {
             libc::openpty(
                 &mut leader,
                 &mut follower,
-                null_mut::<libc::c_char>(),
+                path.as_mut_ptr().cast(),
                 null_mut::<libc::termios>(),
                 null_mut::<libc::winsize>(),
             )
         })?;
 
+        // Get the index of the first null byte and truncate `path` so it doesn't have any null
+        // bytes. If there are no null bytes the path is left as it is.
+        if let Some(index) = path
+            .iter()
+            .enumerate()
+            .find_map(|(index, &byte)| (byte == 0).then_some(index))
+        {
+            path.truncate(index);
+        }
+
+        // This will not panic because `path` was truncated to not have any null bytes.
+        let path = CString::new(path).unwrap();
+
         Ok(Self {
+            path,
             leader: unsafe { OwnedFd::from_raw_fd(leader) },
             follower: unsafe { OwnedFd::from_raw_fd(follower) },
         })
@@ -51,11 +72,24 @@ pub fn tcgetpgrp<F: AsRawFd>(fd: &F) -> io::Result<ProcessId> {
 #[cfg(test)]
 mod tests {
     use std::{
-        io::{Read, Write},
-        os::unix::net::UnixStream,
+        ffi::OsString,
+        io::{IsTerminal, Read, Write},
+        os::unix::{net::UnixStream, prelude::OsStringExt},
+        path::PathBuf,
     };
 
     use crate::system::{fork, getpgid, setsid, term::*};
+
+    #[test]
+    fn open_pty() {
+        let pty = Pty::open().unwrap();
+        assert!(pty.leader.is_terminal());
+        assert!(pty.follower.is_terminal());
+
+        let path = PathBuf::from(OsString::from_vec(pty.path.into_bytes()));
+        assert!(path.try_exists().unwrap());
+        assert!(path.starts_with("/dev/pts/"));
+    }
 
     #[test]
     fn tcsetpgrp_and_tcgetpgrp_are_consistent() {
