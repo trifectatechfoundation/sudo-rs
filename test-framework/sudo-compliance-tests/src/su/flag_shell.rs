0705c3e360b1c4b612d4dd31342f406b6737f850
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-06-29 18:37:32 +0200

    su: more /etc/shells and --shell tests

--- test-framework/sudo-compliance-tests/src/su/flag_shell.rs
+++ test-framework/sudo-compliance-tests/src/su/flag_shell.rs
@@ -161,3 +161,197 @@ echo $0";
 
     Ok(())
 }
+
+#[test]
+fn commented_out_shell_in_etc_shells_is_ignored() -> Result<()> {
+    let invoking_user = USERNAME;
+    let target_user = "ghost";
+    let message = "this is a restricted shell";
+    let restricted_shell_path = "/tmp/restricted-shell";
+    let restricted_shell = format!(
+        "#!/bin/sh
+echo {message}"
+    );
+    let env = Env("")
+        .file(
+            restricted_shell_path,
+            TextFile(restricted_shell).chmod("777"),
+        )
+        .file(
+            "/etc/shells",
+            format!(
+                "# {restricted_shell_path}
+/usr/bin/sh
+/usr/bin/bash"
+            ),
+        )
+        .user(invoking_user)
+        .user(
+            User(target_user)
+                .shell(restricted_shell_path)
+                .password(PASSWORD),
+        )
+        .build()?;
+
+    let output = Command::new("su")
+        .args(["-s", "/usr/bin/false", target_user])
+        .stdin(PASSWORD)
+        .as_user(invoking_user)
+        .output(&env)?;
+
+    assert!(output.status().success(), "{}", output.stderr());
+    assert_contains!(
+        output.stderr(),
+        format!("su: using restricted shell {restricted_shell_path}")
+    );
+
+    assert_eq!(message, output.stdout()?);
+
+    Ok(())
+}
+
+#[test]
+fn when_no_etc_shells_file_uses_a_default_list() -> Result<()> {
+    let default_list = ["/bin/sh"];
+    let not_in_list = [
+        "/bin/bash",
+        "/usr/bin/bash",
+        "/usr/bin/sh",
+        "/bin/dash",
+        "/usr/bin/dash",
+    ];
+    let invoking_user = USERNAME;
+    let target_user = "ghost";
+
+    for shell in not_in_list {
+        eprintln!("out: {shell}");
+
+        let env = Env("")
+            .user(invoking_user)
+            .user(User(target_user).shell(shell).password(PASSWORD))
+            .build()?;
+
+        Command::new("rm")
+            .arg("/etc/shells")
+            .output(&env)?
+            .assert_success()?;
+
+        let output = Command::new("su")
+            .args(["-s", "/usr/bin/false", target_user])
+            .stdin(PASSWORD)
+            .as_user(invoking_user)
+            .output(&env)?;
+
+        assert!(output.status().success(), "{}", output.stderr());
+        assert_contains!(
+            output.stderr(),
+            format!("su: using restricted shell {shell}")
+        );
+    }
+
+    for shell in default_list {
+        eprintln!("in: {shell}");
+
+        let env = Env("")
+            .user(invoking_user)
+            .user(User(target_user).shell(shell).password(PASSWORD))
+            .build()?;
+
+        Command::new("rm")
+            .arg("/etc/shells")
+            .output(&env)?
+            .assert_success()?;
+
+        let output = Command::new("su")
+            .args(["-s", "/usr/bin/true", "-c", "false", target_user])
+            .stdin(PASSWORD)
+            .as_user(invoking_user)
+            .output(&env)?;
+
+        assert!(output.status().success(), "{}", output.stderr());
+        assert_not_contains!(output.stderr(), format!("su: using restricted shell"));
+    }
+
+    Ok(())
+}
+
+#[test]
+fn shell_canonical_path_is_not_used_when_determining_if_shell_is_restricted_or_not() -> Result<()> {
+    let invoking_user = USERNAME;
+    let target_user = "ghost";
+    let shell = "/tmp/bash-symlink";
+
+    let env = Env("")
+        .file("/etc/shells", "/usr/bin/bash")
+        .user(invoking_user)
+        .user(User(target_user).shell(shell).password(PASSWORD))
+        .build()?;
+
+    Command::new("ln")
+        .args(["-s", "/usr/bin/bash", shell])
+        .output(&env)?
+        .assert_success()?;
+
+    Command::new("rm")
+        .arg("/etc/shells")
+        .output(&env)?
+        .assert_success()?;
+
+    let output = Command::new("su")
+        .args(["-s", "/usr/bin/false", target_user])
+        .stdin(PASSWORD)
+        .as_user(invoking_user)
+        .output(&env)?;
+
+    assert!(output.status().success(), "{}", output.stderr());
+    assert_contains!(
+        output.stderr(),
+        format!("su: using restricted shell {shell}")
+    );
+
+    Ok(())
+}
+
+#[test]
+fn shell_is_resolved_with_empty_path_env_var() -> Result<()> {
+    let env = Env("").build()?;
+
+    let command_path = "true";
+    let output = Command::new("su").args(["-s", command_path]).output(&env)?;
+
+    assert!(!output.status().success());
+    assert_eq!(Some(127), output.status().code());
+
+    let diagnostic = if sudo_test::is_original_sudo() {
+        format!("su: failed to execute {command_path}: No such file or directory")
+    } else {
+        format!("su: '{command_path}': command not found")
+    };
+    assert_contains!(output.stderr(), diagnostic);
+
+    Ok(())
+}
+
+#[test]
+fn argument_may_be_a_relative_path() -> Result<()> {
+    let shell_filename = "my-shell";
+    let shell_dir = "/root";
+    let shell = "#!/bin/sh
+echo $0";
+    let env = Env("")
+        .file(
+            format!("{shell_dir}/{shell_filename}"),
+            TextFile(shell).chmod("100"),
+        )
+        .build()?;
+
+    let actual = Command::new("sh")
+        .arg("-c")
+        .arg(format!("cd {shell_dir}; su -s ./{shell_filename}"))
+        .output(&env)?
+        .stdout()?;
+
+    assert_eq!(format!("./{shell_filename}"), actual);
+
+    Ok(())
+}
