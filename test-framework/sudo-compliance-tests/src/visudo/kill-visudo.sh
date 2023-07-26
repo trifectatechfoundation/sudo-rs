c7acce49289940c3c0bf0bd2304e33fdcc96427c
Christian Poveda <christian.poveda@ferrous-systems.com>
2023-07-26 11:33:58 -0500

    Use barrier file in the right way:tm:

--- test-framework/sudo-compliance-tests/src/visudo/kill-visudo.sh
+++ test-framework/sudo-compliance-tests/src/visudo/kill-visudo.sh
@@ -1,8 +1,9 @@
+until [ -f /tmp/barrier ]; do 
+    sleep 0.1
+done
+
 visudopid=$(pidof visudo | sort -gr | cut -f 1 -d ' ')
 
 if [ -n "$visudopid" ]; then
-    until [ -f /tmp/barrier ]; do 
-        sleep 0.1
-    done
     kill $1 "$visudopid"
 fi
