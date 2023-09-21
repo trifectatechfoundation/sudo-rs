4e3166f36c9957ca0f6fd4553540a1c50d8939d2
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-09-21 16:32:52 +0200

    tweak impl to work on 1.70

--- src/macros.rs
+++ src/macros.rs
@@ -50,7 +50,7 @@ macro_rules! cstr {
     ($lit:literal) => {{
         // this `const` item produces compile time errors = it performs the checks at compile time
         const CS: &'static std::ffi::CStr =
-            match std::ffi::CStr::from_bytes_with_nul(concat!($lit, "\0").as_bytes()) {
+            match std::ffi::CStr::from_bytes_until_nul(concat!($lit, "\0").as_bytes()) {
                 Ok(x) => x,
                 Err(_) => panic!("string literal did not pass CStr checks"),
             };
