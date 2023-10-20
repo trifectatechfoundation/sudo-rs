43a24380b2a52f3cacc5857c5605011d01027846
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2023-10-20 15:39:15 +0200

    update usage message

--- src/cli/help.rs
+++ src/cli/help.rs
@@ -1,6 +1,9 @@
 pub const USAGE_MSG: &str = "\
-usage: sudo [-u user] [-g group] [-D directory] [-knS] [-i | -s] <command>
-       sudo -h | -K | -k | -V";
+usage: sudo -h | -K | -k | -V
+usage: sudo -v [-knS] [-g group] [-u user]
+usage: sudo -l [-knS] [-g group] [-U user] [-u user] [command [arg ...]]
+usage: sudo [-knS] [-D directory] [-g group] [-u user] [-i | -s] [command [arg ...]]
+usage: sudo -e [-knS] [-D directory] [-g group] [-u user] file ...";
 
 const DESCRIPTOR: &str = "sudo - run commands as another user";
 
@@ -8,17 +11,17 @@ const HELP_MSG: &str = "Options:
   -D, --chdir=directory         change the working directory before running command
   -g, --group=group             run command as the specified group name or ID
   -h, --help                    display help message and exit
-  -i, --login                   run login shell as the target user; a command may also be
-                                specified
+  -i, --login                   run login shell as the target user; a command may also be specified
   -K, --remove-timestamp        remove timestamp file completely
   -k, --reset-timestamp         invalidate timestamp file
-                                for longer format
+  -l, --list                    list user's privileges or check a specific command; use twice for longer format
   -n, --non-interactive         non-interactive mode, no prompts are used
   -S, --stdin                   read password from standard input
   -s, --shell                   run shell as the target user; a command may also be specified
+  -U, --other-user=user         in list mode, display privileges for user
   -u, --user=user               run command (or edit file) as specified user name or ID
-  -v, --validate                update user's timestamp without running a command
   -V, --version                 display version information and exit
+  -v, --validate                update user's timestamp without running a command
   --                            stop processing command line arguments";
 
 pub fn long_help_message() -> String {
