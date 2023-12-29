7fb43915b9c77f019025e3cb9a6377f1a72e0bc6
blackanger <blackanger.z@gmail.com>
2023-12-29 15:10:39 +0800

    Fixed issues#769 : add test

--- src/system/interface.rs
+++ src/system/interface.rs
@@ -174,6 +174,42 @@ mod test {
 
     use super::*;
 
+    #[test]
+    fn test_user_id() {
+        let uid = UserId(1000);
+        assert_eq!(uid.id(), 1000);
+        let parsed_uid: UserId = "1000".parse().unwrap();
+        assert_eq!(parsed_uid, uid);
+        assert_eq!(format!("{}", uid), "1000");
+    }
+
+    #[test]
+    fn test_group_id() {
+        let gid = GroupId(1000);
+        assert_eq!(gid.id(), 1000);
+        let parsed_gid: GroupId = "1000".parse().unwrap();
+        assert_eq!(parsed_gid, gid);
+        assert_eq!(format!("{}", gid), "1000");
+    }
+
+    #[test]
+    fn test_process_id() {
+        let pid = ProcessId(1000);
+        assert_eq!(pid.id(), 1000);
+        let parsed_pid: ProcessId = "1000".parse().unwrap();
+        assert_eq!(parsed_pid, pid);
+        assert_eq!(format!("{}", pid), "1000");
+    }
+
+    #[test]
+    fn test_device_id() {
+        let did = DeviceId(1000);
+        assert_eq!(did.id(), 1000);
+        let parsed_did: DeviceId = "1000".parse().unwrap();
+        assert_eq!(parsed_did, did);
+        assert_eq!(format!("{}", did), "1000");
+    }
+
     fn test_user(user: impl UnixUser, name_c: &CStr, uid: UserId) {
         let name = name_c.to_str().unwrap();
         assert!(user.has_name(name));
