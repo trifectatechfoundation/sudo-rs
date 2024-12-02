cd0c6b774f11a797482b9fe1a7ee6f5935030598
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-12-02 09:58:18 +0100

    Improve if_set_searches_program_in_secure_path test
    This makes it work on systems where true is in /usr/bin as well as make
    it test what it is supposed to test on systems where /bin is a symlink
    to /usr/bin.

--- test-framework/sudo-compliance-tests/src/sudo/sudoers/secure_path.rs
+++ test-framework/sudo-compliance-tests/src/sudo/sudoers/secure_path.rs
@@ -37,12 +37,11 @@ ALL ALL=(ALL:ALL) NOPASSWD: ALL")
     .file(path, TextFile("#!/bin/sh").chmod("100"))
     .build()?;
 
-    let match_in_relative_path_when_path_is_unset = "unset PATH; cd /bin; /usr/bin/sudo true";
+    // `true` is in `/usr/bin/`
+    let match_in_relative_path_when_path_is_unset = "unset PATH; cd /usr/bin; /usr/bin/sudo true";
     let match_in_absolute_path_when_path_is_unset = "unset PATH; cd /; /usr/bin/sudo my-script";
-    // `true` is in `/usr/bin/true`
-    let match_in_relative_path_when_path_is_set = "export PATH=/usr/bin; cd /bin; sudo true";
-    // default PATH does not include `/root`
-    let match_in_absolute_path_when_path_is_set = "cd /; /usr/bin/sudo my-script";
+    let match_in_relative_path_when_path_is_set = "export PATH=/tmp; cd /usr/bin; /usr/bin/sudo true";
+    let match_in_absolute_path_when_path_is_set = "export PATH=/tmp; cd /; /usr/bin/sudo my-script";
 
     let scripts = [
         match_in_relative_path_when_path_is_unset,
