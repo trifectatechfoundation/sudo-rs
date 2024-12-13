87892fd5fb5313df5c1f765751bfa015ce717e0e
Marc Schoolderman <marc@tweedegolf.com>
2024-12-13 18:55:26 +0100

    add new embedded DSL for writing defaults
    This is more 'statically' typed, e.g. no more need for 'late unwraps'
    whose success can only be determined by careful programming, i.e. a
    defaults::Settings behaves more like Rust-struct whereas the curent
    sudoers::Settings behaves like a Python structs.

--- src/defaults/mod.rs
+++ src/defaults/mod.rs
@@ -10,6 +10,21 @@ pub enum SudoDefault {
     Enum(OptTuple<StrEnum<'static>>),
 }
 
+pub type SettingsModifier = Box<dyn FnOnce(&mut Settings)>;
+
+pub enum ListMode {
+    Set,
+    Add,
+    Del,
+}
+
+pub enum SettingKind {
+    Flag(SettingsModifier),
+    Integer(fn(&str) -> Option<SettingsModifier>),
+    Text(fn(&str) -> Option<SettingsModifier>),
+    List(fn(ListMode, Vec<String>) -> Option<SettingsModifier>),
+}
+
 #[derive(Debug)]
 pub struct OptTuple<T> {
     pub default: T,
@@ -23,6 +38,38 @@ mod settings_dsl;
 use settings_dsl::*;
 
 defaults! {
+    always_query_group_plugin = false
+    always_set_home           = false
+    env_reset                 = true
+    mail_badpass              = true
+    match_group_by_gid        = false
+    use_pty                   = true
+    visiblepw                 = false
+    env_editor                = true
+
+    passwd_tries              = 3 [0..=1000]
+
+    secure_path               = None (!= None)
+
+    verifypw                  = all (!= never) [all, always, any, never]
+
+    timestamp_timeout         = (15*60) (!= 0) {fractional_minutes}
+
+    env_keep                  = ["COLORS", "DISPLAY", "HOSTNAME", "KRB5CCNAME", "LS_COLORS", "PATH",
+                                 "PS1", "PS2", "XAUTHORITY", "XAUTHORIZATION", "XDG_CURRENT_DESKTOP"]
+
+    env_check                 = ["COLORTERM", "LANG", "LANGUAGE", "LC_*", "LINGUAS", "TERM", "TZ"]
+
+    env_delete                = ["IFS", "CDPATH", "LOCALDOMAIN", "RES_OPTIONS", "HOSTALIASES",
+                                "NLSPATH", "PATH_LOCALE", "LD_*", "_RLD*", "TERMINFO", "TERMINFO_DIRS",
+                                "TERMPATH", "TERMCAP", "ENV", "BASH_ENV", "PS4", "GLOBIGNORE",
+                                "BASHOPTS", "SHELLOPTS", "JAVA_TOOL_OPTIONS", "PERLIO_DEBUG",
+                                "PERLLIB", "PERL5LIB", "PERL5OPT", "PERL5DB", "FPATH", "NULLCMD",
+                                "READNULLCMD", "ZDOTDIR", "TMPPREFIX", "PYTHONHOME", "PYTHONPATH",
+                                "PYTHONINSPECT", "PYTHONUSERBASE", "RUBYLIB", "RUBYOPT", "*=()*"]
+}
+
+old_defaults! {
     always_query_group_plugin = false
     always_set_home           = false
     env_reset                 = true
@@ -64,7 +111,7 @@ fn fractional_minutes(input: &str) -> Option<i64> {
 }
 
 #[cfg(test)]
-mod test {
+mod old_test {
     use super::*;
 
     #[test]
@@ -104,5 +151,76 @@ mod test {
 
         let myenum = StrEnum::new("hello", &["hello", "goodbye"]).unwrap();
         assert!(&myenum as &str == "hello");
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[allow(clippy::bool_assert_comparison)]
+    #[test]
+    fn check() {
+        let mut def = Settings::default();
+        assert_eq! { def.always_query_group_plugin, false };
+        assert_eq! { def.always_set_home, false };
+        assert_eq! { def.env_reset, true };
+        assert_eq! { def.mail_badpass, true };
+        assert_eq! { def.match_group_by_gid, false };
+        assert_eq! { def.use_pty, true };
+        assert_eq! { def.visiblepw, false };
+        assert_eq! { def.env_editor, true };
+        assert_eq! { def.passwd_tries, 3 };
+        assert_eq! { def.secure_path, None };
+        assert_eq! { def.env_check, ["COLORTERM", "LANG", "LANGUAGE", "LC_*", "LINGUAS", "TERM", "TZ"].iter().map(|s| s.to_string()).collect() };
+        assert_eq! { def.verifypw, enums::verifypw::all };
+
+        negate("env_check").unwrap()(&mut def);
+        negate("env_reset").unwrap()(&mut def);
+        negate("secure_path").unwrap()(&mut def);
+        negate("verifypw").unwrap()(&mut def);
+        assert_eq! { def.always_query_group_plugin, false };
+        assert_eq! { def.always_set_home, false };
+        assert_eq! { def.env_reset, false };
+        assert_eq! { def.mail_badpass, true };
+        assert_eq! { def.match_group_by_gid, false };
+        assert_eq! { def.use_pty, true };
+        assert_eq! { def.visiblepw, false };
+        assert_eq! { def.env_editor, true };
+        assert_eq! { def.passwd_tries, 3 };
+        assert_eq! { def.secure_path, None };
+        assert! { def.env_check.is_empty() };
+        assert_eq! { def.verifypw, enums::verifypw::never };
+
+        let SettingKind::Flag(f) = set("env_reset").unwrap() else {
+            panic!()
+        };
+        f(&mut def);
+        let SettingKind::Text(f) = set("secure_path").unwrap() else {
+            panic!()
+        };
+        f("/bin").unwrap()(&mut def);
+        let SettingKind::Integer(f) = set("passwd_tries").unwrap() else {
+            panic!()
+        };
+        f("5").unwrap()(&mut def);
+        let SettingKind::Text(f) = set("verifypw").unwrap() else {
+            panic!()
+        };
+        f("any").unwrap()(&mut def);
+        assert_eq! { def.always_query_group_plugin, false };
+        assert_eq! { def.always_set_home, false };
+        assert_eq! { def.env_reset, true };
+        assert_eq! { def.mail_badpass, true };
+        assert_eq! { def.match_group_by_gid, false };
+        assert_eq! { def.use_pty, true };
+        assert_eq! { def.visiblepw, false };
+        assert_eq! { def.env_editor, true };
+        assert_eq! { def.passwd_tries, 5 };
+        assert_eq! { def.secure_path, Some("/bin".into()) };
+        assert! { def.env_check.is_empty() };
+        assert_eq! { def.verifypw, enums::verifypw::any };
+
+        assert!(set("notanoption").is_none());
+        assert!(f("notanoption").is_none());
     }
 }
--- src/defaults/settings_dsl.rs
+++ src/defaults/settings_dsl.rs
@@ -64,7 +64,186 @@ macro_rules! optional {
     };
 }
 
+macro_rules! type_of {
+    ($id:ident, true) => { bool };
+    ($id:ident, false) => { bool };
+    ($id:ident, [ $($value: expr),* ]) => { std::collections::HashSet<String> };
+    ($id:ident, $(=int $check: expr;)+ $_: expr) => { i64 };
+    ($id:ident, $(=enum $k: ident;)+ $_: ident) => { $crate::defaults::enums::$id };
+    ($id:ident, $_: expr) => { Option<Box<str>> };
+}
+
+macro_rules! value_of {
+    ($id:ident, true) => { true };
+    ($id:ident, false) => { false };
+    ($id:ident, [ $($value: expr),* ]) => { [$($value),*].into_iter().map(|s: &str| s.to_string()).collect::<std::collections::HashSet<_>>() };
+    ($id:ident, $(=int $check: expr;)+ $value: expr) => { $value };
+    ($id:ident, $(=enum $k: ident;)+ $value: ident) => { $crate::defaults::enums::$id::$value };
+    ($id:ident, None) => { None };
+    ($id:ident, $value: expr) => { Some($value.into()) };
+    ($id:ident, $($_: tt)*) => { ifdef![] };
+}
+
+macro_rules! negate_of {
+    ($id:ident, true) => {
+        false
+    };
+    ($id:ident, false) => {
+        false
+    };
+    ($id:ident, [ $($value: expr),* ]) => {
+        std::collections::HashSet::new()
+    };
+    ($id:ident, $(=int $check: expr;)+ $value: expr) => {
+        ifdef![]
+    };
+    ($id:ident, $(=enum $k: ident;)+ $value: ident) => {
+        ifdef![]
+    };
+    ($id:ident, None) => {
+        ifdef![]
+    };
+    ($id:ident, $value: expr) => {
+        ifdef![]
+    };
+    ($id:ident, $($_: tt)*) => {
+        ifdef![]
+    };
+}
+
+macro_rules! modifier_of {
+    ($id:ident, true) => {
+        $crate::defaults::SettingKind::Flag(Box::new(move |obj: &mut Settings| obj.$id = true) as _)
+    };
+    ($id:ident, false) => {
+        $crate::defaults::SettingKind::Flag(Box::new(move |obj: &mut Settings| obj.$id = true) as _)
+    };
+    ($id:ident, [ $($value: expr),* ]) => {
+        $crate::defaults::SettingKind::List(
+            |mode, list|
+                Some(Box::new(move |obj: &mut Settings|
+                                       match mode {
+                                          ListMode::Set => obj.$id = list.into_iter().collect(),
+                                          ListMode::Add => obj.$id.extend(list),
+                                          ListMode::Del => for key in [$($value),*] {
+                                                               obj.$id.remove(key);
+                                                           },
+                                       }) as _)
+        )
+    };
+    ($id:ident, =int $first:literal ..= $last: literal $(@ $radix: literal)?; $value: expr) => {
+        #[allow(clippy::from_str_radix_10)]
+        $crate::defaults::SettingKind::Integer(
+            |text| i64::from_str_radix(text, 10$(*0 + $radix)?)
+                            .ok()
+                            .filter(|val| ($first ..= $last)
+                            .contains(val))
+                            .map(|i| Box::new(move |obj: &mut Settings| obj.$id = i) as _)
+        )
+    };
+    ($id:ident, =int $fn: expr; $value: expr) => {
+        $crate::defaults::SettingKind::Integer(
+            |text| $fn(&text).map(|i| Box::new(move |obj: &mut Settings| obj.$id = i) as _)
+        )
+    };
+    ($id:ident, $(=int $check: expr;)+ $value: expr) => { compile_error!("bla") };
+    ($id:ident, $(=enum $key: ident;)+ $value: ident) => {
+        $crate::defaults::SettingKind::Text(|key| match key {
+            $(
+            stringify!($key) => { Some(Box::new(move |obj: &mut Settings| obj.$id = $crate::defaults::enums::$id::$key) as _) },
+            )*
+            _ => None,
+        })
+    };
+    ($id:ident, None) => {
+        $crate::defaults::SettingKind::Text(
+            |text| {
+                let text = text.into();
+                Some(Box::new(move |obj: &mut Settings| obj.$id = Some(text)) as _)
+            }
+        )
+    };
+    ($id:ident, $value: expr) => {
+        $crate::defaults::SettingKind::Text(
+            |text| {
+                let text = text.into();
+                Some(Box::new(move |obj: &mut Settings| obj.$id = Some(text)) as _)
+            }
+        )
+    };
+}
+
+// this macro allows us to help the compiler generate more efficient code in 'fn negate'
+// and enables the way 'fn set' is made
+macro_rules! ifdef {
+    (; $then: expr; $else: expr) => {
+        $else
+    };
+    ($($_: expr)+; $then: expr; $else: expr) => {
+        $then
+    };
+
+    () => {
+        return None
+    };
+    ($body: expr) => {
+        $body
+    };
+}
+
 macro_rules! defaults {
+    ($($name:ident = $value:tt $((!= $negate:tt))? $([$($key:ident),*])? $([$first:literal ..= $last:literal$(; radix: $radix: expr)?])? $({$fn: expr})?)*) => {
+        #[allow(non_camel_case_types)]
+        mod enums {
+            $($(
+                #[derive(Debug)]
+                #[cfg_attr(test, derive(PartialEq, Eq))]
+                pub enum $name { $($key),* }
+            )?)*
+        }
+
+        pub struct Settings {
+            $(pub $name: type_of!($name, $(=int $fn;)?$(=int $first;)?$($(=enum $key;)*)? $value)),*
+        }
+
+        impl Default for Settings {
+            #[allow(unused_parens)]
+            fn default() -> Self {
+                Self {
+                    $($name: value_of!($name, $(=int $fn;)?$(=int $first;)?$($(=enum $key;)*)? $value)),*
+                }
+            }
+        }
+
+        #[allow(clippy::diverging_sub_expression)]
+        #[allow(unreachable_code)]
+        pub fn negate(name: &str) -> Option<SettingsModifier> {
+            match name {
+                $(
+                stringify!($name) if ifdef!($($negate)?; true; false) || matches!(stringify!($value), "true" | "false") || stringify!($value).starts_with('[') => {
+                    let _value = ifdef!($($negate)?;
+                        value_of!($name, $(=int $fn;)?$(=int $first;)?$($(=enum $key;)*)? $($negate)?);
+                        negate_of!($name, $(=int $fn;)?$(=int $first;)?$($(=enum $key;)*)? $value)
+                    );
+                    Some(Box::new(move |obj: &mut Settings| obj.$name = _value))
+                },
+                )*
+                _ => None
+            }
+        }
+
+        pub fn set(name: &str) -> Option<SettingKind> {
+            match name {
+            $(
+                stringify!($name) => Some(modifier_of!($name, $(=int $fn;)?$(=int $first ..= $last $(@ $radix)?;)?$($(=enum $key;)*)? $value)),
+            )*
+                _ => None,
+            }
+        }
+    };
+}
+
+macro_rules! old_defaults {
     ($($name:ident = $value:tt $((!= $negate:tt))? $([$($key:ident),*])? $([$first:literal ..= $last:literal$(; radix: $radix: expr)?])? $({$fn: expr})?)*) => {
         pub const ALL_PARAMS: &'static [&'static str] = &[
             $(stringify!($name)),*
@@ -114,6 +293,12 @@ macro_rules! defaults {
 
 pub(super) use add_conversion;
 pub(super) use defaults;
+pub(super) use ifdef;
+pub(super) use modifier_of;
+pub(super) use negate_of;
+pub(super) use old_defaults;
 pub(super) use optional;
 pub(super) use sliceify;
 pub(super) use tupleify;
+pub(super) use type_of;
+pub(super) use value_of;
