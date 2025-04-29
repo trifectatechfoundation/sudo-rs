d90ed00934792269cc4a98a6d28a18409bd208a0
Marc R. Schoolderman <info@squell.net>
2025-04-29 17:16:23 +0200

    much more readable suffix checking
    Co-authored-by: bjorn3 <17426603+bjorn3@users.noreply.github.com>

--- src/sudoers/tokens.rs
+++ src/sudoers/tokens.rs
@@ -13,13 +13,13 @@ pub struct Username(pub SudoString);
 impl Token for Username {
     fn construct(text: String) -> Result<Self, String> {
         // if a '$' occurs in a username, it has to be the final character
-        if let Some((_, "")) | None = text.split_once('$') {
-            SudoString::new(text)
-                .map_err(|e| e.to_string())
-                .map(Username)
-        } else {
-            Err("embedded $ in username".to_string())
+        if text.strip_suffix('$').unwrap_or(&text).contains('$') {
+            return Err("embedded $ in username".to_string());
         }
+
+        SudoString::new(text)
+            .map_err(|e| e.to_string())
+            .map(Username)
     }
 
     fn accept(c: char) -> bool {
