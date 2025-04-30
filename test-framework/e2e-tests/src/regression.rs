5c784885b938931a456376d9884db143bf560a52
Marc Schoolderman <marc@tweedegolf.com>
2025-04-30 15:21:01 +0200

    add regression test

--- test-framework/e2e-tests/src/regression.rs
+++ test-framework/e2e-tests/src/regression.rs
@@ -1,4 +1,4 @@
-use sudo_test::{Command, Env, TextFile};
+use sudo_test::{Command, Env, TextFile, User};
 
 #[test]
 fn syslog_writer_should_not_hang() {
@@ -29,4 +29,43 @@ fn no_permissions_should_not_violate_io_safety() {
         stderr,
         "sudo-rs: cannot execute '/usr/bin/foo': Permission denied (os error 13)"
     );
+}
+
+#[test]
+fn user_without_permissions_cannot_distinguish_file_existence() {
+    // run test for users both without any permissions and limited permissions that
+    // exclude access to the directory of interest
+    for sudoers in ["", "ALL ALL=/bin/true"] {
+        let env = Env(sudoers)
+            // ordinary users have no access to the directory
+            .directory(sudo_test::Directory("/secret").chmod("777"))
+            // this should be irrespective of file permissions
+            .file("/secret/txt", TextFile("").chmod("000"))
+            .file("/secret/foo", TextFile("#! /bin/sh").chmod("777"))
+            .user(User("user").password("password"))
+            .build();
+
+        for bait in ["/secret/txt", "/secret/foo"] {
+            let output = Command::new("sudo")
+                .args(["-S", "-l", bait])
+                .as_user("user")
+                .stdin("password")
+                .output(&env);
+            assert!(!output.status().success());
+            let response_1 = output.stderr().replace(bait, "<file>");
+
+            let output = Command::new("sudo")
+                .args(["-S", "-l", "/secret/missing"])
+                .as_user("user")
+                .stdin("password")
+                .output(&env);
+            assert!(!output.status().success());
+            let response_2 = output.stderr().replace("/secret/missing", "<file>");
+
+            dbg!(sudoers);
+            dbg!(bait);
+            dbg!(&response_1);
+            assert_eq!(response_1, response_2);
+        }
+    }
 }
