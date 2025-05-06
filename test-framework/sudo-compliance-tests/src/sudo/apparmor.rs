76d9641be973c0b7d8c32fcebc754ab053e2d744
Marc Schoolderman <marc@tweedegolf.com>
2025-05-06 14:33:47 +0200

    add apparmor compliance test

--- test-framework/sudo-compliance-tests/src/sudo/apparmor.rs
+++ test-framework/sudo-compliance-tests/src/sudo/apparmor.rs
@@ -3,7 +3,7 @@ use sudo_test::{Command, Env};
 use crate::Result;
 
 #[test]
-fn switches_the_apparmor_profile() -> Result<()> {
+fn can_switch_the_apparmor_profile() -> Result<()> {
     let env = Env("root ALL=(ALL:ALL) APPARMOR_PROFILE=docker-default ALL")
         .apparmor("unconfined")
         .build();
@@ -29,6 +29,44 @@ fn cannot_switch_to_nonexisting_profile() -> Result<()> {
 
     output.assert_exit_code(1);
     assert_contains!(output.stderr(), "unable to change AppArmor profile");
+
+    Ok(())
+}
+
+#[test]
+fn can_set_default_apparmor_profile() -> Result<()> {
+    let env = Env("root ALL=(ALL:ALL) ALL
+Defaults apparmor_profile=docker-default
+")
+    .apparmor("unconfined")
+    .build();
+
+    let output = Command::new("sudo")
+        .args(["-s", "cat", "/proc/$$/attr/current"])
+        .output(&env);
+    dbg!(&output);
+
+    output.assert_success();
+    assert_eq!(output.stdout(), "docker-default (enforce)");
+
+    Ok(())
+}
+
+#[test]
+fn tags_override_the_default_apparmor_profile() -> Result<()> {
+    let env = Env("root ALL=(ALL:ALL) APPARMOR_PROFILE=unconfined ALL
+Defaults apparmor_profile=docker-default
+")
+    .apparmor("unconfined")
+    .build();
+
+    let output = Command::new("sudo")
+        .args(["-s", "cat", "/proc/$$/attr/current"])
+        .output(&env);
+    dbg!(&output);
+
+    output.assert_success();
+    assert_eq!(output.stdout(), "unconfined");
 
     Ok(())
 }
