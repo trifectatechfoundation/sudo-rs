d8ef68a8183527e98f0c71293db9a2ed5f6070fd
Marc Schoolderman <marc@tweedegolf.com>
2025-05-14 00:22:13 +0200

    ignore the insults

--- src/defaults/mod.rs
+++ src/defaults/mod.rs
@@ -38,7 +38,11 @@ defaults! {
     targetpw                  = false
     noexec                    = false
 
+    insults                   = false  #ignored
+
+    setenv                    = false
     apparmor_profile          = None (!= None)
+
     passwd_tries              = 3 [0..=1000]
 
     secure_path               = None (!= None)
@@ -59,7 +63,6 @@ defaults! {
                                 "PERLLIB", "PERL5LIB", "PERL5OPT", "PERL5DB", "FPATH", "NULLCMD",
                                 "READNULLCMD", "ZDOTDIR", "TMPPREFIX", "PYTHONHOME", "PYTHONPATH",
                                 "PYTHONINSPECT", "PYTHONUSERBASE", "RUBYLIB", "RUBYOPT", "*=()*"] #ignored
-    setenv                    = false
 }
 
 /// A custom parser to parse seconds as fractional "minutes", the format used by
