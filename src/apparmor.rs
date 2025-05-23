407409f83e9676d73dc23f4aeaa48593d9ed73cf
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-05-23 14:17:23 +0200

    Move apparmor::sys into the main apparmor module

--- src/apparmor.rs
+++ src/apparmor.rs
@@ -2,8 +2,6 @@ use std::{ffi::CString, io::ErrorKind};
 
 use crate::cutils::cerr;
 
-mod sys;
-
 /// Set the profile for the next exec call if AppArmor is enabled
 pub fn set_profile_for_next_exec(profile_name: &str) -> std::io::Result<()> {
     if apparmor_is_enabled()? {
@@ -22,11 +20,16 @@ fn apparmor_is_enabled() -> std::io::Result<bool> {
     }
 }
 
+#[link(name = "apparmor")]
+extern "C" {
+    pub fn aa_change_onexec(profile: *const libc::c_char) -> libc::c_int;
+}
+
 /// Switch the apparmor profile to the given profile on the next exec call
 fn apparmor_prepare_exec(new_profile: &str) -> std::io::Result<()> {
     let new_profile_cstr = CString::new(new_profile)?;
     // SAFETY: new_profile_cstr provided by CString ensures a valid ptr
-    cerr(unsafe { sys::aa_change_onexec(new_profile_cstr.as_ptr()) })?;
+    cerr(unsafe { aa_change_onexec(new_profile_cstr.as_ptr()) })?;
 
     Ok(())
 }
