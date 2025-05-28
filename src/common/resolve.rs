4aec3a1bc886314a38080d45ebb4ebfe8f5ffeac
piguycs <piguycs@pm.me>
2025-05-28 15:32:43 +0200

    ignore relative paths when resolving the PATH
    related to: trifectatechfoundation/sudo-rs#1019

--- src/common/resolve.rs
+++ src/common/resolve.rs
@@ -206,45 +206,16 @@ pub(crate) fn is_valid_executable(path: &PathBuf) -> bool {
 /// a regular file and has any executable bits set. It does not specifically
 /// check for user, group, or others' executable bit.
 pub(crate) fn resolve_path(command: &Path, path: &str) -> Option<PathBuf> {
-    // To prevent command spoofing, sudo checks "." and "" (both denoting current directory)
-    // last when searching for a command in the user's PATH (if one or both are in the PATH).
     // Depending on the security policy, the user's PATH environment variable may be modified,
     // replaced, or passed unchanged to the program that sudo executes.
-    let mut resolve_current_path = false;
 
     path.split(':')
-        // register whether to look in the current directory, but first check the other PATH segments
-        .filter(|&path| {
-            if path.is_empty() || path == "." {
-                resolve_current_path = true;
-
-                false
-            } else {
-                true
-            }
-        })
+        // ignore all relative paths ("", "." or "./")
+        .filter(|&path| !(path.is_empty() || path == "." || path.starts_with("./")))
         // construct a possible executable absolute path candidate
         .map(|path| PathBuf::from(path).join(command))
         // check whether the candidate is a regular file and any executable flag is set
         .find(is_valid_executable)
-        // if no no executable could be resolved try the current directory
-        // if it was present in the PATH
-        .or_else(|| {
-            if resolve_current_path {
-                env::current_dir()
-                    .ok()
-                    .map(|dir| dir.join(command))
-                    .and_then(|path| {
-                        if is_valid_executable(&path) {
-                            Some(path)
-                        } else {
-                            None
-                        }
-                    })
-            } else {
-                None
-            }
-        })
 }
 
 #[cfg(test)]
