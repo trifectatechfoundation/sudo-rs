f4443c36a4e30fe2284bb5e859bc3bcca782d88c
Marc Schoolderman <info@squell.net>
2025-12-10 00:09:36 +0100

    update pot file

--- util/make-pot.sh
+++ util/make-pot.sh
@@ -12,7 +12,7 @@ fi
 potfile="$WORK_ROOT/po/sudo-rs.pot"
 test -e "$potfile" && EXISTING="--join-existing --omit-header"
 
-find "$WORK_ROOT" -name "*.rs" -not -name "gettext.rs" | xargs xgettext \
+cd "$WORK_ROOT" && find src -name "*.rs" -not -path "*/gettext/*" | xargs xgettext \
   --package-name="sudo-rs" \
   --package-version="$SUDO_RS_VERSION" \
   --msgid-bugs-address="https://github.com/trifectatechfoundation/sudo-rs/issues" \
