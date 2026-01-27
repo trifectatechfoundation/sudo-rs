08ddbdfcc2d538cf037b3b199e32588b4dd6468c
Marc Schoolderman <marc@tweedegolf.com>
2026-01-28 00:02:46 +0100

    improve test coverage script

---
+++ test-framework/compliance-lcov.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -euo pipefail
+
+test "${1:-}" || (echo "use export, report or show" && false)
+
+# This script requires LLVM-tools to be installed using:
+#   rustup component add llvm-tools
+
+# we need to call llvm-cov and llvm-profdata directly
+PATH="$(rustc --print sysroot)/lib/rustlib/$(rustc --print host-tuple)/bin:$PATH"
+
+# find the project dir relative to the location of this script
+project_dir="$(realpath "${0%/*}/..")"
+
+profdata="$SUDO_TEST_PROFRAW_DIR"/sudo-rs.profdata
+binary="$SUDO_TEST_PROFRAW_DIR"/sudo-rs
+
+llvm-profdata merge \
+	-sparse \
+	"$SUDO_TEST_PROFRAW_DIR"/*/*.profraw \
+	-o "$profdata"
+
+dockerid=$(docker create sudo-test-rs)
+docker cp -q "$dockerid":/usr/bin/sudo "$binary"
+docker rm "$dockerid" > /dev/null
+
+arg="$1"
+shift
+
+case "$arg" in
+export)
+	llvm-cov export \
+		-format=lcov \
+		--ignore-filename-regex='[.]cargo/registry' \
+		--ignore-filename-regex='/usr/local/cargo/registry' \
+		--ignore-filename-regex='/rustc' \
+		--instr-profile="$profdata" \
+		--object "$binary" \
+                "$@" \
+                | sed "s:/usr/src/sudo:$project_dir:g";;
+
+report)
+	llvm-cov report \
+		--color \
+		--ignore-filename-regex='[.]cargo/registry' \
+		--ignore-filename-regex='/usr/local/cargo/registry' \
+		--ignore-filename-regex='/rustc' \
+		--instr-profile="$profdata" \
+		--object "$binary" \
+		-path-equivalence="/usr/src/sudo,$project_dir" \
+                "$@";;
+
+show)
+	llvm-cov show \
+		--color \
+		--ignore-filename-regex='[.]cargo/registry' \
+		--ignore-filename-regex='/usr/local/cargo/registry' \
+		--ignore-filename-regex='/rustc' \
+		--instr-profile="$profdata" \
+		--object "$binary" \
+		-path-equivalence="/usr/src/sudo,$project_dir" \
+                "$@";;
+
+*)
+	echo "unknown llvm-cov command: use export, report or show"
+	exit 1;;
+esac
