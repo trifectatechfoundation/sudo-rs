f451fe33b14aacf1a9bc2ea9daef91e64f88d9db
Marc Schoolderman <marc@tweedegolf.com>
2026-04-07 20:11:33 +0200

    improvements to bash script

--- test-framework/compliance-lcov.sh
+++ test-framework/compliance-lcov.sh
@@ -16,9 +16,13 @@ project_dir="$(realpath "${0%/*}/..")"
 profdata="$SUDO_TEST_PROFRAW_DIR"/sudo-rs.profdata
 binary="$SUDO_TEST_PROFRAW_DIR"/sudo-rs
 
+# to merge coverage data from unit tests is only possible with 'export'
+# also, the function coverage will become flawed
+
 llvm-profdata merge \
 	-sparse \
 	"$SUDO_TEST_PROFRAW_DIR"/*/*.profraw \
+	$(find "$project_dir"/target/llvm-cov-target -maxdepth 1 -type f -name "*.profraw" -print 2> /dev/null) \
 	-o "$profdata"
 
 dockerid=$(docker create sudo-test-rs)
@@ -35,8 +39,13 @@ export)
 		--ignore-filename-regex='[.]cargo/registry' \
 		--ignore-filename-regex='/usr/local/cargo/registry' \
 		--ignore-filename-regex='/rustc' \
+		--ignore-filename-regex='src/bin' \
+		--ignore-filename-regex='sudoers/test' \
+		--ignore-filename-regex='tests[.]rs' \
+		--ignore-filename-regex='gettext/check[.]rs' \
 		--instr-profile="$profdata" \
 		--object "$binary" \
+                $(find "$project_dir"/target/llvm-cov-target/debug/deps -maxdepth 1 -type f -executable -printf '--object %p\n') \
                 "$@" \
                 | sed "s:/usr/src/sudo:$project_dir:g";;
 
@@ -46,6 +55,10 @@ report)
 		--ignore-filename-regex='[.]cargo/registry' \
 		--ignore-filename-regex='/usr/local/cargo/registry' \
 		--ignore-filename-regex='/rustc' \
+		--ignore-filename-regex='src/bin' \
+		--ignore-filename-regex='sudoers/test' \
+		--ignore-filename-regex='tests[.]rs' \
+		--ignore-filename-regex='gettext/check[.]rs' \
 		--instr-profile="$profdata" \
 		--object "$binary" \
 		-path-equivalence="/usr/src/sudo,$project_dir" \
@@ -57,6 +70,10 @@ show)
 		--ignore-filename-regex='[.]cargo/registry' \
 		--ignore-filename-regex='/usr/local/cargo/registry' \
 		--ignore-filename-regex='/rustc' \
+		--ignore-filename-regex='src/bin' \
+		--ignore-filename-regex='sudoers/test' \
+		--ignore-filename-regex='tests[.]rs' \
+		--ignore-filename-regex='gettext/check[.]rs' \
 		--instr-profile="$profdata" \
 		--object "$binary" \
 		-path-equivalence="/usr/src/sudo,$project_dir" \
