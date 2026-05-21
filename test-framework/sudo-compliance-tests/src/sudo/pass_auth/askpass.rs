1177af06c734134040d6a98030fad489a90744ab
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-05-21 21:26:37 +0200

    pam: Adjust error message to match sudo.ws on missing tty
    In case no TTY is available, sudo suggests using `-S` option, while we
    do not do it in sudo-rs.
    
    Use the same logic of sudo.ws here.

--- test-framework/sudo-compliance-tests/src/sudo/pass_auth/askpass.rs
+++ test-framework/sudo-compliance-tests/src/sudo/pass_auth/askpass.rs
@@ -128,9 +128,9 @@ fn no_tty_does_not_use_askpass_without_display() {
 
     output.assert_exit_code(1);
     let diagnostic = if sudo_test::is_original_sudo() {
-        "a terminal is required to read the password"
+        "a terminal is required to read the password; either use the -S option to read from standard input or configure an askpass helper"
     } else {
-        "A terminal is required to authenticate"
+        "A terminal is required to authenticate; either use the -S option to read the password from standard input or configure an askpass helper"
     };
     assert_contains!(output.stderr(), diagnostic);
 }
@@ -148,9 +148,9 @@ fn no_tty_with_display_but_without_askpass_still_fails() {
 
     output.assert_exit_code(1);
     let diagnostic = if sudo_test::is_original_sudo() {
-        "a terminal is required to read the password"
+        "a terminal is required to read the password; either use the -S option to read from standard input or configure an askpass helper"
     } else {
-        "A terminal is required to authenticate"
+        "A terminal is required to authenticate; either use the -S option to read the password from standard input or configure an askpass helper"
     };
     assert_contains!(output.stderr(), diagnostic);
 }
